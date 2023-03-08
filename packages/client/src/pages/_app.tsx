1ca95ee582ff3e60e4b05b072bf949031c99c484
Joshua Grierson <joshuagrierson@bluelightcard.co.uk>
2023-03-08 16:33:16 +0000

    feat: regional based feature toggle environments

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,32 +1,20 @@
 import type { AppProps } from 'next/app';
-import { FC, PropsWithChildren } from 'react';
+import { FC } from 'react';
 import { appWithTranslation } from 'next-i18next';
 import { FlagsmithProvider } from 'flagsmith/react';
 import flagsmith from 'flagsmith/isomorphic';
+import { BRAND, REGION } from 'global-vars';
 
-import featureToggleState from '../feature-toggles.json';
 import '../styles/main.scss';
 
-const userToEmail: Record<string, string> = {
-  user_1: 'user_1@bluelightcard.co.uk',
-  user_2: 'user_2@bluelightcard.co.uk',
-  user_3: 'user_3@bluelightcard.co.uk',
-};
+const featureToggleState = require(`../../brands/${BRAND}/featureToggleConfigs/${REGION}.json`);
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-  let userId: string = 'user_1';
-  if (typeof window !== 'undefined' && sessionStorage.getItem('userId')) {
-    userId = `user_${sessionStorage.getItem('userId') as string}`;
-  }
   return (
     <FlagsmithProvider
       options={{
         environmentID: featureToggleState.environmentID,
         state: featureToggleState,
-        traits: {
-          email_address: userToEmail[userId],
-        },
-        identity: userId,
       }}
       flagsmith={flagsmith}
     >
--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,47 +1,9 @@
 import { NextPage } from 'next';
 import { Button } from 'react-bootstrap';
 import { useTranslation } from 'next-i18next';
-import * as yup from 'yup';
 import getI18nStaticProps from '@/utils/i18nStaticProps';
-import CreateAccountLayout from '@/layouts/CreateAccountLayout/CreateAccountLayout';
-import InputTextField from '@/components/InputTextField/InputTextField';
-import { InputTextFieldProps } from '@/components/InputTextField/types';
-import Form from '@/components/Form/Form';
 
 export const getStaticProps = getI18nStaticProps;
-
-const fields: any = [
-  [
-    {
-      label: 'First name',
-      controlId: 'firstNameFieldControl',
-      required: true,
-      message: 'Provide your first name',
-      fieldComponent: InputTextField,
-    },
-    {
-      label: 'Last name',
-      controlId: 'lastNameFieldControl',
-      required: true,
-      message: 'Provide your last name',
-      fieldComponent: InputTextField,
-    },
-  ],
-  {
-    label: 'Email',
-    controlId: 'emailFieldControl',
-    required: true,
-    message: 'Provide a valid email address',
-    validation: yup
-      .string()
-      .email('Email address is a required field')
-      .required('Email address is required'),
-    fieldComponent: InputTextField,
-    fieldComponentProps: {
-      type: 'email',
-    } as InputTextFieldProps,
-  },
-];
 
 const Home: NextPage<any> = () => {
   const { t } = useTranslation('common');
@@ -49,9 +11,6 @@ const Home: NextPage<any> = () => {
     <main>
       <h1>{t('heading')}</h1>
       <Button variant="primary">Primary Button</Button>
-      <CreateAccountLayout>
-        <Form fields={fields} onSubmit={() => console.info('Submitted')} />
-      </CreateAccountLayout>
     </main>
   );
 };

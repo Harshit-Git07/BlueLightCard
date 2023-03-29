b98cb73317541dfec7b2d51bda8e84f05f0c368b
Joshua Grierson <joshuagrierson@bluelightcard.co.uk>
2023-03-29 12:20:27 +0100

    test: fixed lint issues

--- packages/client/src/common/components/__tests__/Button/Button.test.tsx
+++ packages/client/src/common/components/__tests__/Button/Button.test.tsx
@@ -3,7 +3,7 @@ import { ButtonProps } from '@/components/Button/types';
 import { faMinus, faPlus } from '@fortawesome/pro-solid-svg-icons';
 import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
-import 'jest-styled-components'
+import 'jest-styled-components';
 
 describe('Button component', () => {
   let props: ButtonProps;
@@ -26,17 +26,13 @@ describe('Button component', () => {
 
   describe('snapshot Test', () => {
     it('renders a button with the correct icon side "left"', () => {
-      const component = renderer.create(
-        <Button text="Button" iconLeft={faMinus}/>,
-      );
+      const component = renderer.create(<Button text="Button" iconLeft={faMinus} />);
       const tree = component.toJSON();
       expect(tree).toMatchSnapshot();
     });
 
     it('renders a button with the correct icon side "right"', () => {
-      const component = renderer.create(
-        <Button text="Button" iconRight={faPlus}/>,
-      );
+      const component = renderer.create(<Button text="Button" iconRight={faPlus} />);
       const tree = component.toJSON();
       expect(tree).toMatchSnapshot();
     });

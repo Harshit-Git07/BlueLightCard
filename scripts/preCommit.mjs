323fb84d4ec0301b732fae9f7edf8ba122393f8c
Joshua Grierson <joshuagrierson@bluelightcard.co.uk>
2023-03-31 16:48:04 +0100

    test: check if result is set

--- scripts/preCommit.mjs
+++ scripts/preCommit.mjs
@@ -4,16 +4,16 @@ import { EOL } from 'os';
 try {
   const result = execSync('git diff --name-only --cached').toString().trim();
 
-  if (!result.length) {
+  if (result.length) {
+    const paths = result.split(EOL).map((path) => path.replace('packages/client/', ''));
+    if (paths.length) {
+      execSync(`npx jest --passWithNoTests --findRelatedTests ${paths.join(' ')}`, {
+        cwd: 'packages/client',
+        stdio: 'inherit',
+      });
+    }
+  } else {
     console.info('No staged files found');
-  }
-
-  const paths = result.split(EOL).map((path) => path.replace('packages/client/', ''));
-  if (paths.length) {
-    execSync(`npx jest --passWithNoTests --findRelatedTests ${paths.join(' ')}`, {
-      cwd: 'packages/client',
-      stdio: 'inherit',
-    });
   }
 } catch (error) {
   console.error(error);

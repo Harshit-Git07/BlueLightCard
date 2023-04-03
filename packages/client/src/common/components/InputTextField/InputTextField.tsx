4bd1553d4b9954065b25db9dfb192fd2c1320450
Joshua Grierson <joshuagrierson@bluelightcard.co.uk>
2023-04-03 11:57:23 +0100

    test: trigger related tests on PR

--- packages/client/src/common/components/InputTextField/InputTextField.tsx
+++ packages/client/src/common/components/InputTextField/InputTextField.tsx
@@ -38,6 +38,7 @@ const InputTextField: FC<InputTextFieldProps> = ({
       onTogglePasswordVisible(!isPasswordVisible);
     }
   };
+  // comment
   return (
     <InputFieldWrapper
       icon={icon}

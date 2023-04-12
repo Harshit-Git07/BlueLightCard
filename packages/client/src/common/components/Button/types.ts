9529cebfd5d9b0ac1721e9506d306b83e7e2afe1
Muhammad Raza <MuhammadRaza@bluelightcard.co.uk>
2023-04-12 16:09:36 +0100

    refactor: change ButtonProps alias

--- packages/client/src/common/components/Button/types.ts
+++ packages/client/src/common/components/Button/types.ts
@@ -1,11 +1,11 @@
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
-import { ButtonProps as ReactPillButtonProps } from 'react-bootstrap/Button';
+import { ButtonProps as ReactButtonProps } from 'react-bootstrap/Button';
 
 export interface StyledButtonIconProps {
   side: 'left' | 'right';
 }
 
-export type ButtonProps = ReactPillButtonProps & {
+export type ButtonProps = ReactButtonProps & {
   text: string;
   iconLeft?: IconDefinition;
   iconRight?: IconDefinition;

0ad90d4ae91af3ae7b99a351ddb38fc9418f2760
gwalton-blc <130578138+gwalton-blc@users.noreply.github.com>
2023-06-14 13:04:20 +0100

    feat(client): inputelectfield added to identity/component (#50)
    * feat(client): inputselectfield component added
    
    * feat(client): inputselectfield component added to identity/components folder
    
    * feat(client): tab index now prop as per joshgriersonblc's ammendments
    
    * feat(client): story altered to match other identity components

---
+++ packages/client/src/identity/components/InputSelectField/InputSelectField.stories.tsx
@@ -0,0 +1,86 @@
+import { faEnvelope } from '@fortawesome/pro-solid-svg-icons/faEnvelope';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { Meta, StoryFn } from '@storybook/react';
+import InputSelectFieldWithRef from './InputSelectField';
+
+const icons = { faEnvelope };
+
+library.add(...Object.values(icons));
+
+const iconArgSelect = {
+  name: 'Icon',
+  options: ['none'].concat(...Object.keys(icons)),
+  mapping: { none: undefined, ...icons },
+  control: {
+    type: 'select',
+    labels: {
+      none: 'No Icon',
+      faEnvelope: 'Envelope Icon',
+      faLock: 'Password Icon',
+    },
+  },
+};
+
+const componentMeta: Meta<typeof InputSelectFieldWithRef> = {
+  title: 'Component System/identity/InputSelectField',
+  component: InputSelectFieldWithRef,
+  argTypes: {
+    icon: {
+      description: 'Icon appears left of the select',
+      ...iconArgSelect,
+    },
+    options: {
+      name: 'Select Options',
+      description: 'Add/Edit options to display in the select dropdown',
+      control: {
+        type: 'object',
+      },
+    },
+    defaultOption: {
+      description: 'Default option when no option selected',
+    },
+    onChange: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+};
+
+const InputSelectFieldTemplate: StoryFn<typeof InputSelectFieldWithRef> = (args) => (
+  <InputSelectFieldWithRef {...args} />
+);
+
+export const Default = InputSelectFieldTemplate.bind({});
+
+Default.args = {
+  options: [
+    { key: 1, value: 'Option One' },
+    { key: 2, value: 'Option Two' },
+  ],
+  defaultOption: 'Default Option',
+};
+
+export const Success = InputSelectFieldTemplate.bind({});
+
+Success.args = {
+  options: [
+    { key: 1, value: 'Option One' },
+    { key: 2, value: 'Option Two' },
+  ],
+  defaultOption: 'Default Option',
+  success: true,
+};
+
+export const Error = InputSelectFieldTemplate.bind({});
+
+Error.args = {
+  options: [
+    { key: 1, value: 'Option One' },
+    { key: 2, value: 'Option Two' },
+  ],
+  defaultOption: 'Default Option',
+  error: true,
+};
+
+export default componentMeta;
---
+++ packages/client/src/identity/components/InputSelectField/InputSelectField.tsx
@@ -0,0 +1,50 @@
+import { FC, forwardRef } from 'react';
+import InputFieldWrapper from '@/components/_shared/InputFieldWrapper';
+import { InputSelectFieldProps, KeyValue } from './types';
+
+const InputSelectField: FC<InputSelectFieldProps> = ({
+  icon,
+  options,
+  error,
+  success,
+  value,
+  defaultOption,
+  onChange,
+  tabIndex,
+}) => {
+  const selectStyleVariants = {
+    error:
+      'border-semantic-danger-base w-full rounded-md py-2 px-3 border-neutrals-type-1-400 border focus:outline-none',
+    no_error:
+      'focus:border-primary-type-1-base w-full rounded-md py-2 px-3 border-neutrals-type-1-400 border focus:outline-none',
+    icon: 'pl-8',
+  };
+  return (
+    <InputFieldWrapper icon={icon} select={true} showSuccessState={success} showErrorState={error}>
+      <select
+        aria-label="drop-down selector"
+        tabIndex={tabIndex}
+        className={
+          (icon ? selectStyleVariants.icon : '') +
+          (error ? selectStyleVariants.error : selectStyleVariants.no_error)
+        }
+        value={value}
+        onChange={onChange}
+      >
+        {defaultOption && <option>{defaultOption}</option>}
+        {options?.map((option: KeyValue) => (
+          <option aria-label="drop-down option" tabIndex={0} key={option.key} value={option.value}>
+            {option.value}
+          </option>
+        ))}
+      </select>
+    </InputFieldWrapper>
+  );
+};
+
+// eslint-disable-next-line react/display-name
+const InputSelectFieldWithRef = forwardRef<unknown, InputSelectFieldProps>((props, ref) => (
+  <InputSelectField {...props} _ref={ref} />
+));
+
+export default InputSelectFieldWithRef;

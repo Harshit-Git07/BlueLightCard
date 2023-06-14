84ee2b138c71bd5484bb664ca8dafb789857c5e0
ssalehin-blc <130645288+ssalehin-blc@users.noreply.github.com>
2023-06-14 14:38:44 +0100

    feat(web): infocard component created (#81)

---
+++ packages/web/src/identity/components/InfoCard/InfoCard.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react';
+import { InfoCardProps } from './Types';
+import Image from 'next/image';
+import { ASSET_PREFIX } from '@/global-vars';
+
+const InfoCard: FC<InfoCardProps> = ({
+  title,
+  text,
+  imageSrc,
+  imageSizes,
+  assetPrefix = ASSET_PREFIX,
+}) => {
+  return (
+    <div className="rounded-lg overflow-hidden shadow-md max-w-[300px]">
+      {imageSrc && (
+        <div className="relative pb-[50%]">
+          <Image
+            className="object-cover text-center"
+            src={`${assetPrefix}/${imageSrc}`}
+            alt="image"
+            unoptimized
+            fill
+            sizes={imageSizes}
+          />
+        </div>
+      )}
+      <div className="p-4">
+        <h4 className="text-xl mb-2 font-semibold text-center text-infoCard-text-title">{title}</h4>
+        <p className="mb-2 text-center text-infoCard-text-p">{text}</p>
+      </div>
+    </div>
+  );
+};
+
+export default InfoCard;

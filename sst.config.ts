4e5697bdfbb9a730fc2502872023c79da703bcee
lhalan <134288776+lhalan@users.noreply.github.com>
2023-07-11 13:03:47 +0100

    fix: bring back apigateway V1 (#162)

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,7 @@
 import { SSTConfig } from "sst";
 import { Identity } from "./packages/api/identity/stack";
 import { Offers } from "./packages/api/offers/stack";
+import { Web } from "./packages/web/stack";
 import { Shared } from "./stacks/stack";
 
 export default {
@@ -22,6 +23,7 @@ export default {
       .stack(Shared, { id: "global" })
       .stack(Offers, { id: "offers" })
       .stack(Identity, { id: "identity" })
+      .stack(Web, { id: "web" })
   },
 } satisfies SSTConfig;
 

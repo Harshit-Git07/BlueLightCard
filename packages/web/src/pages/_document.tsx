f891479b640930031bb36472bd96df9d037fda7a
gwalton-blc <130578138+gwalton-blc@users.noreply.github.com>
2023-08-15 16:38:29 +0100

    feat(web): ga-snippets (#262)
    * feat(web): google analytics snippet added into _document.tsx
    
    * feat(web): script loading strategy changed to beforeInteractive

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 import { FC } from 'react';
 import NewRelicTag from '@/components/NewRelicTag/NewRelicTag';
 import { DEFAULT_LANG } from '@/global-vars';
-
+import Script from 'next/script';
 const newRelicLicenseKey = process.env.NEXT_PUBLIC_NEWRELIC_LICENSE_KEY as string;
 const newRelicApplicationId = process.env.NEXT_PUBLIC_NEWRELIC_APPLICATION_ID as string;
 
@@ -10,11 +10,28 @@ const AppDocument: FC = () => {
   return (
     <Html lang={DEFAULT_LANG}>
       <Head>
+        <Script id="google-analytics-script" strategy="beforeInteractive">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WW7M2P5');
+        `}
+        </Script>
         {!!(newRelicLicenseKey && newRelicApplicationId) && (
           <NewRelicTag licenseKey={newRelicLicenseKey} applicationID={newRelicApplicationId} />
         )}
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WW7M2P5"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>

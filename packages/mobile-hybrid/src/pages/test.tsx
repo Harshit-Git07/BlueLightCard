d0712c49d9b2c1500424afc1bb854d8424477997
Joshua Grierson <120564550+joshuagriersonblc@users.noreply.github.com>
2023-08-24 14:14:18 +0100

    feat: mobile/setup dev deploy pipeline (#304)
    * ci: initial
    
    * ci: change
    
    * ci: change
    
    * ci: change
    
    * ci: change
    
    * ci: change
    
    * ci: change
    
    * chore: test comment
    
    * ci: removed unessecary extra condition
    
    * chore: removed commented out code
    
    ---------
    
    Co-authored-by: Muhammad Raza <MuhammadRaza@bluelightcard.co.uk>

--- packages/mobile-hybrid/src/pages/test.tsx
+++ packages/mobile-hybrid/src/pages/test.tsx
@@ -1,14 +1,5 @@
 import Head from 'next/head';
 import { NextPage } from 'next';
-import ExploreLink from '@/components/ExploreLink/ExploreLink';
-import {
-  faTag,
-  faCompass,
-  faGiftCard,
-  faThumbsUp,
-  faAward,
-  faSignsPost,
-} from '@fortawesome/pro-light-svg-icons';
 import Search from '@/components/Search/Search';
 
 const TestPage: NextPage<any> = () => {
@@ -18,12 +9,6 @@ const TestPage: NextPage<any> = () => {
         <title>Mobile Hybrid</title>
         <meta name="description" />
       </Head>
-      {/* <ExploreLink icon={faTag} title={'High Street'} />
-      <ExploreLink icon={faCompass} title={'Online'} />
-      <ExploreLink icon={faGiftCard} title={'Giftcards'} />
-      <ExploreLink icon={faThumbsUp} title={'Popular'} />
-      <ExploreLink icon={faAward} title={'Featured'} />
-      <ExploreLink icon={faSignsPost} title={'Local Services'} /> */}
       <Search onSearch={() => {}} />
     </>
   );

8eb52475f00063732eef72fcdf0389d6d5f0bbd7
Muhammad Raza <56206247+illusiveCode@users.noreply.github.com>
2023-09-01 17:09:31 +0100

    feat(mobile): bt 2880 add search to app page (#321)
    * feat: add search on homepage
    
    * chore: add missing slash
    
    * chore: test
    
    * chore: update mobile hybrid actions
    
    * chore: fix search implementation
    
    * chore: change search url string
    
    * chore: uncomment loader
    
    * chore: hide search bar
    
    * chore: add search bar to homepage view
    
    * chore: update searchbar functionality
    
    ---------
    
    Co-authored-by: Muhammad Raza <MuhammadRaza@bluelightcard.co.uk>

--- packages/mobile-hybrid/src/components/Search/Search.tsx
+++ packages/mobile-hybrid/src/components/Search/Search.tsx
@@ -1,14 +1,24 @@
-import React, { FC, FormEventHandler, useRef } from 'react';
+import React, { FC, FormEventHandler, useRef, useState } from 'react';
 import { SearchProps } from './types';
 import { faSearch } from '@fortawesome/pro-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Search: FC<SearchProps> = ({ onSearch, labelText }) => {
   const initialValue = useRef<HTMLInputElement>(null);
-  const onSubmit: FormEventHandler<HTMLFormElement> = () => {
-    const searchTerm = initialValue.current?.value.trim() ?? '';
-    onSearch(searchTerm);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    const searchTerm = initialValue.current?.value.trim();
+
+    if (searchTerm && searchTerm.length >= 2) {
+      setErrorMessage('');
+      onSearch(searchTerm);
+    } else {
+      setErrorMessage('Enter 2 or more characters to search.');
+    }
   };
+
   return (
     <form onSubmit={onSubmit} className="mx-4 my-2">
       <div className="relative">
@@ -26,6 +36,7 @@ const Search: FC<SearchProps> = ({ onSearch, labelText }) => {
           className="px-12 py-3 font-museo rounded-full w-full overflow-x-hidden bg-neutral-grey-100 border-neutral-grey-200 border dark:text-white dark:border-neutral-700 dark:bg-neutral-grey-800 focus:outline-none"
         />
       </div>
+      {errorMessage && <div className="text-red-500">{errorMessage}</div>}
     </form>
   );
 };
--- packages/mobile-hybrid/src/pages/test.tsx
+++ packages/mobile-hybrid/src/pages/test.tsx
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 import { NextPage } from 'next';
 import Search from '@/components/Search/Search';
+import InvokeNativeNavigation from '@/invoke/navigation';
 
+const navigation = new InvokeNativeNavigation();
 const TestPage: NextPage<any> = () => {
   return (
     <>
@@ -9,8 +11,13 @@ const TestPage: NextPage<any> = () => {
         <title>Mobile Hybrid</title>
         <meta name="description" />
       </Head>
-      <Search onSearch={() => {}} />
-      {/* //test comment */}
+      <Search
+        onSearch={(searchTerm) =>
+          navigation.navigate(
+            `/offers.php?type=1&opensearch=1&search=${encodeURIComponent(searchTerm)}`,
+          )
+        }
+      />
     </>
   );
 };

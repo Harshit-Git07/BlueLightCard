ad7f3849e67085844b8f2fe082d2a6246f8c36fe
Muhammad Raza <56206247+illusiveCode@users.noreply.github.com>
2023-09-12 13:54:48 +0100

    feat(mobile): remove experiment calls (#348)
    * feat: invoke native experiments
    
    * feat: add receive section for experiment
    
    * feat: implement requests to native for experiment
    
    * chore: change function values
    
    * chore: hide promobanner
    
    * test: receive experiments
    
    * chore: update expriment conditions
    
    * chore: update subscriber value
    
    * chore: android experiment update
    
    * chore: add correct experiment names
    
    * chore: add console logs
    
    * chore: parse received data for experiments
    
    * chore: change types for experiment
    
    * chore: add dynamic rendering for experiment variants
    
    * chore: uncomment loader
    
    * chore: change variable to a constant
    
    * chore: add promobanner back
    
    * chore: hide experiemnts
    
    * chore: remove experiment request call
    
    * test: empty experiments
    
    * chore: remove experiment call
    
    ---------
    
    Co-authored-by: Muhammad Raza <MuhammadRaza@bluelightcard.co.uk>

--- packages/mobile-hybrid/src/pages/index.tsx
+++ packages/mobile-hybrid/src/pages/index.tsx
@@ -51,22 +51,11 @@ const Home: NextPage<any> = () => {
     }
     apiCall.requestData('/api/4/offer/promos_new.php');
     apiCall.requestData('/api/4/news/list.php');
-    experiments.experiment(['ios-test-experiment', 'android-test-experiment']);
   }, []);
 
   return (
     <main ref={bodyHeight}>
       <div className="mb-9">
-        {expr['android-test-experiment'] != null && (
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded m-2">
-            {expr['android-test-experiment']}
-          </button>
-        )}
-        {expr['ios-test-experiment'] != null && (
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">
-            {expr['ios-test-experiment']}
-          </button>
-        )}
         <PromoBanner />
         <Offers />
         <Heading title="Explore" size="small" />

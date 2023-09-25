76dde05e594a957a4fdfc966e9e3dc935f6854af
Cain Ashmore <cain.ashmore@gmail.com>
2023-09-25 19:07:08 +0100

    fix: gql endpoint (#381)
    * Added gql end points to web.yml
    
    * added offers end point to pr envs
    
    * Updated getCDNUrl link to use correct definition of CDN_URL which contains fallback

--- packages/web/src/common/utils/getCDNUrl.ts
+++ packages/web/src/common/utils/getCDNUrl.ts
@@ -1,11 +1,11 @@
+import { CDN_URL } from '@/global-vars';
+
 /**
  * Get CDN url - Converts a relative path to a CDN url into an absolute path
  * @param relativePath string - A relative path to the file in the CDN
  * @returns string - The CDN url as an absolute path
  */
-function getCDNUrl(relativePath: string) {
-  const cdn = process.env.NEXT_PUBLIC_APP_CDN_URL ?? '';
-  return cdn + (relativePath.startsWith('/') ? '' : '/') + relativePath;
-}
+const getCDNUrl = (relativePath: string) =>
+  CDN_URL + (relativePath.startsWith('/') ? '' : '/') + relativePath;
 
 export default getCDNUrl;

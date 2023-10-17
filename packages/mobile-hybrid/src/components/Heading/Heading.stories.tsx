d7022639d1e6f9ffa8c259a1240013ccdc4d8d10
Muhammad Raza <56206247+illusiveCode@users.noreply.github.com>
2023-10-17 12:55:25 +0100

    feat(mobile): bt 3153 install storybook (#496)
    * chore: hide search on live
    
    * chore: re add search
    
    * feat: add storybook to mobile hybrid package
    
    * chore: fix linting errors
    
    ---------
    
    Co-authored-by: Muhammad Raza <MuhammadRaza@bluelightcard.co.uk>

---
+++ packages/mobile-hybrid/src/components/Heading/Heading.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryFn } from '@storybook/react';
+import Heading from './Heading';
+
+const componentMeta: Meta<typeof Heading> = {
+  title: 'Component System/Heading',
+  component: Heading,
+  argTypes: { title: { control: 'text' }, size: { control: 'radio' } },
+};
+
+const DefaultTemplate: StoryFn<typeof Heading> = (args) => (
+  <Heading {...args}>Storyboard Heading</Heading>
+);
+
+export const Default = DefaultTemplate.bind({});
+
+Default.args = {
+  title: 'Heading Text',
+  size: 'small',
+};
+
+export const Medium = DefaultTemplate.bind({});
+Medium.args = {
+  title: 'Heading Text',
+  size: 'medium',
+};
+
+export default componentMeta;

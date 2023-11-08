cd234af6b3d9ac77b10e587db9ea0a766f7c4ff8
luke-johnson-blc <141424704+luke-johnson-blc@users.noreply.github.com>
2023-11-08 10:40:09 +0000

    fix: update tags to create chore (#612)
    * fix: update readme to create chore
    
    * fix: remove unrequired semi colons

--- packages/api/offers/src/constructs/tags.ts
+++ packages/api/offers/src/constructs/tags.ts
@@ -1,8 +1,8 @@
 import { Stack } from 'aws-cdk-lib'
 export class Tags {
   constructor(private stack: Stack) {
-    stack.tags.setTag('service', 'offers');
-    stack.tags.setTag('map-migrated', 'd-server-017zxazumgiycz');
+    stack.tags.setTag('service', 'offers')
+    stack.tags.setTag('map-migrated', 'd-server-017zxazumgiycz')
   }
 
 }

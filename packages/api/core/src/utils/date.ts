76bcd422e1f54aeb3e796c8daf0a83776fc0fd96
limrubi <52201491+limrubi@users.noreply.github.com>
2023-11-08 16:54:19 +0000

    fix: cater to older default values (#622)
    Co-authored-by: rlimbu <rlimbu@bluelightcard.co.uk>

--- packages/api/core/src/utils/date.ts
+++ packages/api/core/src/utils/date.ts
@@ -3,5 +3,8 @@ export function convertDateUnix(date: string) {
 }
 
 export function transformDateToFormatYYYYMMDD(date: string | null) {
+    if(date === '0000-00-00'){
+        return null;
+    }
     return (date !== null && date.trim().length > 0) ? new Date(date).toISOString().substring(0,10) : null;
 }

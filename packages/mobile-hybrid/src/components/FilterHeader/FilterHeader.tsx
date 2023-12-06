ed333ddeb6fa95f428bc60f7410cbd57601166cf
Muhammad Raza <56206247+illusiveCode@users.noreply.github.com>
2023-12-06 16:02:28 +0000

    feat(mobile): bt 3462 filter sheet header (#718)
    * add lock file
    
    * feat: add filter header component
    
    * chore: remove click functions

---
+++ packages/mobile-hybrid/src/components/FilterHeader/FilterHeader.tsx
@@ -0,0 +1,33 @@
+import React, { FC, useState } from 'react';
+import { FilterHeaderProps } from './types';
+import { cssUtil } from '@/utils/cssUtil';
+
+const FilterHeader: FC<FilterHeaderProps> = ({ onDoneClick, onResetClick, resetEnabled }) => {
+  const resetButtonClasses = cssUtil([
+    resetEnabled
+      ? 'enabled:text-primary-dukeblue-600 cursor-pointer dark:text-primary-vividskyblue-700'
+      : 'disabled text-neutral-grey-600 dark:text-neutral-grey-400 cursor-not-allowed',
+    'px-4',
+  ]);
+
+  return (
+    <div className="rounded-t-3xl h-[56px] dark:bg-neutral-grey-800 font-museo flex justify-between">
+      <button disabled={!resetEnabled} onClick={onResetClick} className={resetButtonClasses}>
+        Reset
+      </button>
+      <div className="flex items-center">
+        <h3 className="text-xl text-neutral-oxfordblue-900 dark:text-primary-vividskyblue-700 font-semibold">
+          Filter
+        </h3>
+      </div>
+      <button
+        onClick={onDoneClick}
+        className="px-4 text-primary-dukeblue-600 dark:text-primary-vividskyblue-700"
+      >
+        Done
+      </button>
+    </div>
+  );
+};
+
+export default FilterHeader;

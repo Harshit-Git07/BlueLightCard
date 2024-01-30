466886571fe062581663617d2d166935a5051b41
Ben Heidemann <56122437+bcheidemann@users.noreply.github.com>
2024-01-30 15:45:18 +0000

    fix(redemptions): database seeding fk violations (#812)

--- packages/api/redemptions/src/database/seed/seed.ts
+++ packages/api/redemptions/src/database/seed/seed.ts
@@ -13,7 +13,7 @@ import {
 } from '../schema';
 
 export async function seed({ db }: DatabaseConnection): Promise<void> {
-  const redemptionsData: (typeof redemptions.$inferInsert)[] = [
+  const redemptionsData = [
     {
       id: `${redemptionsPrefix}-6db7a3c8-13a8-4cf3-a072-9b08ecd9d534`,
       affiliate: 'awin',
@@ -25,22 +25,22 @@ export async function seed({ db }: DatabaseConnection): Promise<void> {
       redemptionType: 'vault',
       url: 'https://awin1.com/',
     },
-  ];
+  ] satisfies (typeof redemptions.$inferInsert)[];
   await db.insert(redemptions).values(redemptionsData).onConflictDoNothing();
 
   const genericsData: (typeof generics.$inferInsert)[] = [
     {
       id: `${genericsPrefix}-b5761799-d552-4496-a46e-8ff43162dbdb`,
-      redemptionId: '6db7a3c8-13a8-4cf3-a072-9b08ecd9d534',
+      redemptionId: redemptionsData[0].id,
       code: 'BLC25OFF',
     },
   ];
   await db.insert(generics).values(genericsData).onConflictDoNothing();
 
-  const vaultsData: (typeof vaults.$inferInsert)[] = [
+  const vaultsData = [
     {
       id: `${vaultsPrefix}-bbe8558f-dc4b-447a-bc62-381755c70174`,
-      redemptionId: '6db7a3c8-13a8-4cf3-a072-9b08ecd9d534',
+      redemptionId: redemptionsData[0].id,
       alertBelow: 10,
       created: new Date('2024-01-05T14:48:00.000Z'),
       email: 'admin@bluelightcard.co.uk',
@@ -50,28 +50,28 @@ export async function seed({ db }: DatabaseConnection): Promise<void> {
       terms:
         'Cannot be used in conjunction with any other online and/or instore promotion. Excludes clearance lines. Online offer only.',
     },
-  ];
+  ] satisfies (typeof vaults.$inferInsert)[];
   await db.insert(vaults).values(vaultsData).onConflictDoNothing();
 
-  const vaultBatchesData: (typeof vaultBatches.$inferInsert)[] = [
+  const vaultBatchesData = [
     {
       id: `${vaultBatchesPrefix}-e4dafac0-2ebf-479e-9936-8d75a4780be4`,
-      vaultId: 'bbe8558f-dc4b-447a-bc62-381755c70174',
+      vaultId: vaultsData[0].id,
       file: 'codes.csv',
     },
-  ];
+  ] satisfies (typeof vaultBatches.$inferInsert)[];
   await db.insert(vaultBatches).values(vaultBatchesData).onConflictDoNothing();
 
-  const vaultCodesData: (typeof vaultCodes.$inferInsert)[] = [
+  const vaultCodesData = [
     {
       id: `${vaultCodesPrefix}-b8f674c3-c2c8-4cd6-bc62-1688bb4e62fd`,
-      vaultId: 'bbe8558f-dc4b-447a-bc62-381755c70174',
-      batchId: 'e4dafac0-2ebf-479e-9936-8d75a4780be4',
+      vaultId: vaultsData[0].id,
+      batchId: vaultBatchesData[0].id,
       code: 'BLC25OFF-EXE123',
       created: new Date('2024-01-05T14:48:00.000Z'),
       expiry: new Date('2025-01-05T14:48:00.000Z'),
       memberId: '1234',
     },
-  ];
+  ] satisfies (typeof vaultCodes.$inferInsert)[];
   await db.insert(vaultCodes).values(vaultCodesData).onConflictDoNothing();
 }

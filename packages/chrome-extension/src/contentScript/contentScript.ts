874f94bb77e0133afce649f617cca50fe4cc90e3
Luke Halan <64604760+lukehalan@users.noreply.github.com>
2024-02-27 12:10:54 +0000

    fix: chrome ext quick fix to stop ext to remove search results (#927)
    * fix: remove google dom injection
    
    * fix: increment the version
    
    * fix: remove commented code
    
    ---------
    
    Co-authored-by: Luke Halan <lukehalan@bluelightcard.co.uk>

--- packages/chrome-extension/src/contentScript/contentScript.ts
+++ packages/chrome-extension/src/contentScript/contentScript.ts
@@ -1,34 +1,34 @@
 import {
-  injectGoogleSearchResultNotification,
+  //injectGoogleSearchResultNotification,
   injectDiscountReceivedNotification,
   injectNotification,
-} from "../utils/helpers";
-import {createUrlWithGA4CampaignQueryParams} from "../utils/googleAnalytics";
+} from '../utils/helpers';
+import { createUrlWithGA4CampaignQueryParams } from '../utils/googleAnalytics';
 
 const cleanUrlString = (url: string) => {
-  let cleanedUrl = url.replace(/https?:\/\/(www\.)?/, "").split(/[ ›]/)[0];
+  let cleanedUrl = url.replace(/https?:\/\/(www\.)?/, '').split(/[ ›]/)[0];
 
-  const parts = cleanedUrl.split(".");
+  const parts = cleanedUrl.split('.');
 
-  const ccSLDs = ["co.uk", "com.au", "co.nz", "co.za", "com.sg"];
+  const ccSLDs = ['co.uk', 'com.au', 'co.nz', 'co.za', 'com.sg'];
 
   const domainEndsInCcSLD = ccSLDs.some((ccSLD) => cleanedUrl.endsWith(ccSLD));
 
   if (domainEndsInCcSLD) {
-    return parts.slice(-3).join(".");
+    return parts.slice(-3).join('.');
   } else {
-    return parts.slice(-2).join(".");
+    return parts.slice(-2).join('.');
   }
 };
 
 let urls: string[] = [];
 let allElements: Element[] = [];
 
-const adParentElements = document.querySelectorAll(".qGXjvb");
+const adParentElements = document.querySelectorAll('.qGXjvb');
 
 adParentElements.forEach((parent) => {
   allElements.push(parent);
-  const citeElements = parent.querySelectorAll(".x2VHCd");
+  const citeElements = parent.querySelectorAll('.x2VHCd');
   citeElements.forEach((element) => {
     //@ts-ignore
     const url = element.dataset.dtld;
@@ -38,21 +38,11 @@ adParentElements.forEach((parent) => {
   });
 });
 
-const parentElements = document.querySelectorAll(".MjjYud");
-
-parentElements.forEach((parent) => {
-  const peopleAlsoAskSection = parent.querySelector(".cUnQKe");
-  const relatedSearchesSection = parent.querySelector(".oIk2Cb");
-  const videoSection = parent.querySelector(".uVMCKf");
-
-  if (peopleAlsoAskSection || relatedSearchesSection || videoSection) {
-    parent.remove();
-  }
-});
+const parentElements = document.querySelectorAll('.MjjYud');
 
 parentElements.forEach((parent) => {
   allElements.push(parent);
-  const citeElements = parent.querySelectorAll("cite");
+  const citeElements = parent.querySelectorAll('cite');
   citeElements.forEach((cite) => {
     const url = cite.textContent;
     if (url) {
@@ -61,36 +51,6 @@ parentElements.forEach((parent) => {
     }
   });
 });
-
-chrome.runtime.sendMessage(
-  { message: "scanUrls", urls: urls },
-  function (response) {
-    allElements.forEach((parent, idx) => {
-      response.matchedUrls.map((item: { url: string; idx: number }) => {
-        if (item.idx === idx) {
-          injectGoogleSearchResultNotification(parent);
-
-          const table = parent.querySelector("table");
-          if (table) {
-            const dtElements = table.querySelectorAll("td");
-            dtElements.forEach((dt) => {
-              injectGoogleSearchResultNotification(dt);
-            });
-          }
-
-          const secondLevelElements = parent.querySelector(".UBEOKe");
-          if (secondLevelElements) {
-            const secondLevelDivs =
-              secondLevelElements.querySelectorAll(".MhgNwc");
-            secondLevelDivs.forEach((div) => {
-              injectGoogleSearchResultNotification(div);
-            });
-          }
-        }
-      });
-    });
-  }
-);
 
 chrome.runtime.onMessage.addListener((request) => {
   if (request.isDiscountReceived) {

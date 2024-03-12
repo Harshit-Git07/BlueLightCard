9a7a59425be31a3129775f13aa59edfdbe7e59a1
luis-blc <160228623+luis-blc@users.noreply.github.com>
2024-03-12 09:32:16 +0000

    fix: fix storybook, adding remaining option for pills (#1026)
    * created a first draft for the new pill component
    
    * finished pill component for companys page
    
    * added comment to code
    
    * added pill outline version
    
    * added requested varints for pills

--- packages/web/src/common/components/PillButtons/PillButtons.stories.tsx
+++ packages/web/src/common/components/PillButtons/PillButtons.stories.tsx
@@ -42,6 +42,41 @@ PillWithIconRight.args = {
   iconRight: <FontAwesomeIcon size="sm" icon={faCog} />,
 };
 
+export const PillWithIconLeftDisabled = Template.bind({});
+
+PillWithIconLeftDisabled.args = {
+  text: 'Pill',
+  onSelected: () => void 0,
+  disabled: true,
+  iconLeft: <FontAwesomeIcon size="sm" icon={faCog} />,
+};
+
+export const PillWithIconRightDisabled = Template.bind({});
+
+PillWithIconRightDisabled.args = {
+  text: 'Pill',
+  onSelected: () => void 0,
+  disabled: true,
+  iconRight: <FontAwesomeIcon size="sm" icon={faCog} />,
+};
+
+export const PillOutlineWithIconRight = Template.bind({});
+
+PillOutlineWithIconRight.args = {
+  text: 'Pill',
+  outline: true,
+  iconRight: <FontAwesomeIcon size="sm" icon={faCog} />,
+  onSelected: () => void 0,
+};
+export const PillOutlineWithIconLeft = Template.bind({});
+
+PillOutlineWithIconLeft.args = {
+  text: 'Pill',
+  iconLeft: <FontAwesomeIcon size="sm" icon={faCog} />,
+  outline: true,
+  onSelected: () => void 0,
+};
+
 export const PillOutline = Template.bind({});
 
 PillOutline.args = {

b67874180cb8e5a9ea31606ebe623fdfa125f6e8
rajat sharma <63790985+rajat-sg@users.noreply.github.com>
2024-04-02 17:38:07 +0530

    refactor: TO-650 updatd offers API tests (#1083)
    * refactor: TO-650 updatd offers API tests
    
    * refactor: TO-650 fixed test
    
    * refactor: TO-650 updated offers API tests to test for image in response
    
    ---------
    
    Co-authored-by: Rajat Sharma <>

--- packages/api/offers/src/routes/offers/__tests__/getOfferHandler.test.ts
+++ packages/api/offers/src/routes/offers/__tests__/getOfferHandler.test.ts
@@ -1,19 +1,57 @@
-import { handler } from './../getOfferHandler';
 import axios from 'axios';
-import { HttpStatusCode } from '../../../../../core/src/types/http-status-code.enum';
 import { APIGatewayEvent } from 'aws-lambda';
-import mockLegacyAPIResponse from './mocks/legacyOffersMockAPIResponse.json';
-import notFoundLegacyMockAPIResponse from './mocks/notFoundLegacyMockAPIResponse.json';
-import getOfferMockAPIResponse from './mocks/getOffersMockAPIResponse.json';
-import nullExpiryLegacyMockAPIResponse from './mocks/legacyOffersMockAPIResponseNullExpiry.json';
-import invalidExpiryLegacyMockAPIResponse from './mocks/legacyOffersMockAPIResponseInvalidExpiry.json';
-import mockLambdaEvent from './mocks/lambdaMockEvent.json';
-import multipleOffersLegacyMockAPIResponse from './mocks/legacyOffersMockAPIResponseMultipleOffers.json';
-import noMatchingOfferLegacyMockAPIResponse from './mocks/noMatchLegacyOffersMockAPIResponseMultipleOffers.json';
+import { HttpStatusCode } from '../../../../../core/src/types/http-status-code.enum';
+import { handler } from './../getOfferHandler';
+import { OFFERS_TYPE_ENUM } from '../../../utils/global-constants';
+import { LegacyOffers } from '../../../models/legacy/legacyOffers';
+import { mockLegacyOfferRetrieveAPI } from '../../../mocks/legacyOfferRetrieveAPI';
+import { Offer } from '../../../models/offers';
+import { mockLambdaEvent } from '../../../mocks/lambdaEvent';
+
 jest.mock('axios');
 jest.mock('../../../utils/getLegacyUserIdFromToken', () => ({
   getLegacyUserId: jest.fn().mockReturnValue('1'),
 }));
+jest.mock('../../../../../core/src/utils/getEnv', () => ({
+  getEnv: jest.fn().mockImplementation((param) => {
+    if (param === 'service') {
+      return 'test';
+    }
+  }),
+}));
+
+const validateSuccessfulResponse = (offerId: number, result: any, legacyAPIMockResponseData: any) => {
+  expect(result.statusCode).toEqual(HttpStatusCode.OK);
+  const APIResponseBody = JSON.parse(result.body);
+  expect(APIResponseBody.message).toEqual('Success');
+  expect(APIResponseBody).toHaveProperty('data');
+
+  const offer: Offer = APIResponseBody.data;
+  const companyId = legacyAPIMockResponseData.id;
+  const companyLogo = legacyAPIMockResponseData.s3logos;
+  const legacyOffers = legacyAPIMockResponseData.offers;
+  const legacyOffer: LegacyOffers = legacyOffers.find((legacyOffer: LegacyOffers) => offerId === legacyOffer.id);
+
+  expect(legacyOffer).toHaveProperty('id');
+  expect(legacyOffer.id).toEqual(offerId);
+  if (legacyOffer) {
+    expect(offer['id']).toEqual(offerId);
+    expect(offer['name']).toEqual(legacyOffer['name']);
+    expect(offer['description']).toEqual(legacyOffer['desc']);
+    expect(offer['terms']).toEqual(legacyOffer['terms']);
+    expect(offer['image']).toEqual(legacyOffer['imageoffer']);
+    expect(offer['companyId']).toEqual(companyId);
+    expect(offer['companyLogo']).toEqual(companyLogo);
+    const validType = offer.type in OFFERS_TYPE_ENUM.enum;
+    expect(validType).toBeTruthy();
+
+    if (legacyOffer.expires && !isNaN(new Date(legacyOffer.expires).valueOf())) {
+      expect(new Date(offer.expiry as Date)).toStrictEqual(legacyOffer.expires);
+    } else {
+      expect(offer).not.toHaveProperty('expiry');
+    }
+  }
+};
 
 describe('handler', () => {
   beforeAll(() => {
@@ -21,175 +59,146 @@ describe('handler', () => {
   });
 
   it('should return a successful response when the event is valid', async () => {
-    axios.get = jest.fn().mockResolvedValue(mockLegacyAPIResponse);
-    process.env.LEGACY_RETRIEVE_OFFERS_URL = 'https://example.com/legacy-offers';
+    const legacyAPIMockResponse = mockLegacyOfferRetrieveAPI(1);
+    axios.get = jest.fn().mockResolvedValue(legacyAPIMockResponse);
+    const offerId = legacyAPIMockResponse.data.data.offers[0].id;
+
     const event: APIGatewayEvent = {
-      ...mockLambdaEvent,
+      ...mockLambdaEvent('/offers/{id}', 'GET'),
       pathParameters: {
-        id: '1234',
+        id: offerId.toString(),
       },
     };
 
     const result = await handler(event);
-
-    expect(result.statusCode).toEqual(HttpStatusCode.OK);
-    expect(result.body).toEqual(JSON.stringify(getOfferMockAPIResponse));
+    validateSuccessfulResponse(offerId, result, legacyAPIMockResponse.data.data);
   });
 
   it('should return a successful response when legacy returns multiple offers', async () => {
-    axios.get = jest.fn().mockResolvedValue(multipleOffersLegacyMockAPIResponse);
-    process.env.LEGACY_RETRIEVE_OFFERS_URL = 'https://example.com/legacy-offers';
+    const legacyAPIMockResponse = mockLegacyOfferRetrieveAPI(3);
+    axios.get = jest.fn().mockResolvedValue(legacyAPIMockResponse);
+    const offerId = legacyAPIMockResponse.data.data.offers[0].id;
+
     const event: APIGatewayEvent = {
-      ...mockLambdaEvent,
+      ...mockLambdaEvent('/offers/{id}', 'GET'),
       pathParameters: {
-        id: '1234',
+        id: offerId.toString(),
       },
     };
 
     const result = await handler(event);
-
-    expect(result.statusCode).toEqual(HttpStatusCode.OK);
-    expect(result.body).toEqual(JSON.stringify(getOfferMockAPIResponse));
+    validateSuccessfulResponse(offerId, result, legacyAPIMockResponse.data.data);
   });
 
   it('should return a successful response when the legacy API returns expiry as null', async () => {
-    axios.get = jest.fn().mockResolvedValue(nullExpiryLegacyMockAPIResponse);
-    process.env.LEGACY_RETRIEVE_OFFERS_URL = 'https://example.com/legacy-offers';
+    const legacyAPIMockResponse = mockLegacyOfferRetrieveAPI(1);
+    legacyAPIMockResponse.data.data.offers[0].expires = null;
+    axios.get = jest.fn().mockResolvedValue(legacyAPIMockResponse);
+    const offerId = legacyAPIMockResponse.data.data.offers[0].id;
+
     const event: APIGatewayEvent = {
-      ...mockLambdaEvent,
+      ...mockLambdaEvent('/offers/{id}', 'GET'),
       pathParameters: {
-        id: '1234',
+        id: offerId.toString(),
       },
     };
+
     const result = await handler(event);
-    console.log(result);
-    const mockAPIResponse = {
-      message: 'Success',
-      data: {
-        id: 1234,
-        name: 'offer name',
-        description: 'description',
-        type: 'Online',
-        terms: 'tnc',
-        image: "https://www.example.com/image.png",
-        companyId: 3456,
-        companyLogo: 'https://www.example.com/logo.png',
-      },
-    };
-    expect(result.statusCode).toEqual(HttpStatusCode.OK);
-    expect(result.body).toEqual(JSON.stringify(mockAPIResponse));
+    validateSuccessfulResponse(offerId, result, legacyAPIMockResponse.data.data);
   });
 
   it('should return a successful response when the legacy API returns expiry as invalid date', async () => {
-    axios.get = jest.fn().mockResolvedValue(invalidExpiryLegacyMockAPIResponse);
-    process.env.LEGACY_RETRIEVE_OFFERS_URL = 'https://example.com/legacy-offers';
+    const legacyAPIMockResponse = mockLegacyOfferRetrieveAPI(1);
+    legacyAPIMockResponse.data.data.offers[0].expires = '0000-00-00T00:00:00';
+    axios.get = jest.fn().mockResolvedValue(legacyAPIMockResponse);
+    const offerId = legacyAPIMockResponse.data.data.offers[0].id;
+
     const event: APIGatewayEvent = {
-      ...mockLambdaEvent,
+      ...mockLambdaEvent('/offers/{id}', 'GET'),
       pathParameters: {
-        id: '1234',
+        id: offerId.toString(),
       },
     };
+
     const result = await handler(event);
-    const mockAPIResponse = {
-      message: 'Success',
-      data: {
-        id: 1234,
-        name: 'offer name',
-        description: 'description',
-        type: 'Online',
-        terms: 'tnc',
-        image: "https://www.example.com/image.png",
-        companyId: 3456,
-        companyLogo: 'https://www.example.com/logo.png',
-      },
-    };
-    expect(result.statusCode).toEqual(HttpStatusCode.OK);
-    expect(result.body).toEqual(JSON.stringify(mockAPIResponse));
+    validateSuccessfulResponse(offerId, result, legacyAPIMockResponse.data.data);
   });
 
   it('should return a not found error response when the event is valid but legacy returns no offer', async () => {
-    axios.get = jest.fn().mockResolvedValue(notFoundLegacyMockAPIResponse);
-    process.env.LEGACY_RETRIEVE_OFFERS_URL = 'https://example.com/legacy-offers';
+    const legacyAPIMockResponse = mockLegacyOfferRetrieveAPI(0);
+    axios.get = jest.fn().mockResolvedValue(legacyAPIMockResponse);
+
     const event: APIGatewayEvent = {
-      ...mockLambdaEvent,
+      ...mockLambdaEvent('/offers/{id}', 'GET'),
       pathParameters: {
-        id: '1234',
+        id: '123',
       },
     };
 
     const result = await handler(event);
-
     expect(result.statusCode).toEqual(HttpStatusCode.NOT_FOUND);
     expect(result.body).toEqual(JSON.stringify({ message: 'Offer not found', data: {} }));
   });
 
   it('should return a not found error response when the event is valid but offer is not present in multiple offers from legacy', async () => {
-    axios.get = jest.fn().mockResolvedValue(noMatchingOfferLegacyMockAPIResponse);
-    process.env.LEGACY_RETRIEVE_OFFERS_URL = 'https://example.com/legacy-offers';
+    const legacyAPIMockResponse = mockLegacyOfferRetrieveAPI(2);
+    axios.get = jest.fn().mockResolvedValue(legacyAPIMockResponse);
+    legacyAPIMockResponse.data.data.offers[0].id = 10;
+    legacyAPIMockResponse.data.data.offers[1].id = 101; // to avoid matching the offer id in params with 1010
     const event: APIGatewayEvent = {
-      ...mockLambdaEvent,
+      ...mockLambdaEvent('/offers/{id}', 'GET'),
       pathParameters: {
-        id: '1234',
+        id: '1010',
       },
     };
 
-    const result = await handler(event).catch((error) => {
-      console.log(error);
-    });
-
+    const result = await handler(event);
     expect(result.statusCode).toEqual(HttpStatusCode.NOT_FOUND);
     expect(result.body).toEqual(JSON.stringify({ message: 'Offer not found', data: {} }));
   });
 
   it('should return an error response when the legacy api returns an error', async () => {
     axios.get = jest.fn().mockRejectedValue({ message: 'Error fetching offers' });
-    process.env.LEGACY_RETRIEVE_OFFERS_URL = 'https://example.com/legacy-offers';
+
     const event: APIGatewayEvent = {
-      ...mockLambdaEvent,
+      ...mockLambdaEvent('/offers/{id}', 'GET'),
       pathParameters: {
         id: '1234',
       },
     };
 
-    handler(event).catch((error) => {
-      expect(error.statusCode).toEqual(HttpStatusCode.INTERNAL_SERVER_ERROR);
-      expect(error.body).toEqual(
-        JSON.stringify({
-          message: 'Error',
-          error: 'Error fetching offers',
-        }),
-      );
-    });
+    const error = await handler(event);
+
+    expect(error.statusCode).toEqual(HttpStatusCode.INTERNAL_SERVER_ERROR);
+    expect(error.body).toEqual(
+      JSON.stringify({
+        message: 'Error',
+        error: 'Error fetching offers',
+      }),
+    );
   });
 
   it('should return an error response when json response does not match required data structure', async () => {
-    axios.get = jest.fn().mockResolvedValue({
-      ...mockLegacyAPIResponse,
-      data: {
-        ...mockLegacyAPIResponse.data,
-        data: {
-          ...mockLegacyAPIResponse.data.data,
-          id: 'abc',
-        },
-      },
-    });
-    process.env.LEGACY_RETRIEVE_OFFERS_URL = 'https://example.com/legacy-offers';
+    const legacyAPIMockResponse = mockLegacyOfferRetrieveAPI(1);
+    axios.get = jest.fn().mockResolvedValue(legacyAPIMockResponse);
+    const offerId = legacyAPIMockResponse.data.data.offers[0].id;
+    legacyAPIMockResponse.data.data.offers[0].name = 123;
+
     const event: APIGatewayEvent = {
-      ...mockLambdaEvent,
+      ...mockLambdaEvent('/offers/{id}', 'GET'),
       pathParameters: {
-        id: '1234',
+        id: offerId.toString(),
       },
     };
 
-    handler(event).catch((error) => {
-      console.log(error);
-      expect(error.statusCode).toEqual(HttpStatusCode.INTERNAL_SERVER_ERROR);
-      expect(error.body).toEqual(
-        JSON.stringify({
-          message: 'Error',
-          error: 'Error fetching offers',
-        }),
-      );
-    });
+    const error = await handler(event);
+
+    expect(error.statusCode).toEqual(HttpStatusCode.INTERNAL_SERVER_ERROR);
+    expect(error.body).toEqual(
+      JSON.stringify({
+        message: 'Error',
+        error: 'Error fetching offers',
+      }),
+    );
   });
 });

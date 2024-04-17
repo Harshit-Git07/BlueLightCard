f3b39983fac2d9b099b84c5c098873c8e1a03a50
Ricky Luu <ricky.luu911@gmail.com>
2024-04-17 10:27:27 +0100

    add base64decode (#1196)
    Co-authored-by: ricky.luu <ricky.luu@bluelight.co.uk>

--- packages/web/src/common/utils/redirect.ts
+++ packages/web/src/common/utils/redirect.ts
@@ -1,9 +1,11 @@
 import { NextRouter } from 'next/router';
+import { decodeBase64 } from '@/utils/base64';
+
 export const redirect = (redirectURL: string, milliseconds: number, router: NextRouter) => {
   return new Promise<void>((resolve, reject) => {
     setTimeout(async () => {
       try {
-        await router.push(redirectURL);
+        await router.push(decodeBase64(redirectURL));
         resolve();
       } catch (error) {
         reject();

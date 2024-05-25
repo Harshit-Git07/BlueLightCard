1b3362b2ae684419b4b7444b2204b3b44fd56df5
Chris Gedrim <chris@gedrim.co.uk>
2024-05-25 18:32:22 +0100

    feat: fix copy (#1450)
    Co-authored-by: Chris Gedrim <chrisgedrim@blue lightcard.co.uk>

--- packages/api/identity/src/cognito/preAuthentication.ts
+++ packages/api/identity/src/cognito/preAuthentication.ts
@@ -15,7 +15,7 @@ const WRONG_PASSWORD_RESET_TRIGGER_MINUTES:number = Number(process.env.WRONG_PAS
 
 // const UNSUCCESSFUL_ATTEMPTS_CUSTOM_ERROR_MESSAGE = ":\n You previously had " + WRONG_PASSWORD_ENTER_LIMIT + " unsuccessful login attempts for this email in the last " + WRONG_PASSWORD_RESET_TRIGGER_MINUTES +" minutes.\n\nAs a precaution, your current login attempt has been blocked.\n\nIf your email exists in our system, you will receive a password reset email in your inbox";
 const UNSUCCESSFUL_ATTEMPTS_CUSTOM_ERROR_MESSAGE = `:
-It looks like you've entered an incorrect email address or password.
+It looks like you have entered an incorrect email address or password.
 
 If your email exists in our system you will receive a password reset email in your inbox.
 

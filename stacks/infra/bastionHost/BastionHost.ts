1fa1c756162f8bdc5c96e41365a021c6b1f98a8c
Ben Heidemann <benheidemann@bluelightcard.co.uk>
2024-06-13 15:40:46 +0100

    feat: add IAM user with DB access (#1468)
    Co-authored-by: Ferenc Kovacs <ferenckovacs@bluelightcard.co.uk>

--- stacks/infra/bastionHost/BastionHost.ts
+++ stacks/infra/bastionHost/BastionHost.ts
@@ -5,11 +5,21 @@ import { IBastionHostAdapter } from './adapter';
 import { isEphemeral, isProduction, isStaging } from '@blc-mono/core/src/utils/checkEnvironment';
 import { getEnvRaw } from '@blc-mono/core/utils/getEnv';
 import { SharedStackEnvironmentKeys } from '../environment';
+import { BastionHostAccessUser } from './BastionHostAccessUser';
 
+/**
+ * **IMPORTANT:** The bastion host allows production access to databases via a
+ *                set of shared credentails. DO NOT USE IT if your database
+ *                contains sensitive information of any kind.
+ * 
+ * @deprecated Please read the above note carefully.
+ */
 export class BastionHost {
   constructor(private readonly stack: Stack, private readonly vpc: IVpc) {}
 
   public setup(): IBastionHostAdapter | undefined {
+    // Only create the bastion host for staging and production environments,
+    // unless the DANGEROUSLY_ALLOW_SHARED_BASTION_HOST flag is set.
     if (
       !getEnvRaw(SharedStackEnvironmentKeys.DANGEROUSLY_ALLOW_SHARED_BASTION_HOST)
       && !isProduction(this.stack.stage)
@@ -21,6 +31,7 @@ export class BastionHost {
 
     const defaultSecurityGroup = this.createDefaultSecurityGroup();
     const bastionHost = this.createBastionHost(defaultSecurityGroup);
+    this.createBastionHostAccessUser(bastionHost);
     return this.createBastionHostAdapter(bastionHost, defaultSecurityGroup);
   }
 
@@ -44,6 +55,20 @@ export class BastionHost {
       value: host.instanceId,
     });
     return host;
+  }
+
+  private createBastionHostAccessUser(bastionHost: BastionHostLinux) {
+    // Only create the user for staging and production environments, unless the
+    // DANGEROUSLY_ALLOW_DB_ACCESS_USER_CREATION flag is set.
+    if (
+      !getEnvRaw(SharedStackEnvironmentKeys.DANGEROUSLY_ALLOW_DB_ACCESS_USER_CREATION) &&
+      !isProduction(this.stack.stage) &&
+      !isStaging(this.stack.stage)
+    ) {
+      return;
+    }
+
+    new BastionHostAccessUser(this.stack, bastionHost).setup();
   }
 
   private createBastionHostAdapter(

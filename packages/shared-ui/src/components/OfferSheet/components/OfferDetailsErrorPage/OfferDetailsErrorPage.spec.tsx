078a74707f6d358d370294ea65d7025376a3b51c
Harry Wright <harrywright@bluelightcard.co.uk>
2024-06-13 16:30:37 +0100

    fix: TO 781 company cta redirects to non existent offer page when offer not found (#1587)
    * fix: fix navigation to company page on offer sheet error
    
    * test: added tests to check navigation to company page
    
    * chore: update package lock
    
    * Revert "chore: update package lock"
    
    This reverts commit 3d3f43df73858a7a89dafd9ab1e8ea4ed194d77e.
    
    * fix: actually revert package-lock file

--- packages/shared-ui/src/components/OfferSheet/components/OfferDetailsErrorPage/OfferDetailsErrorPage.spec.tsx
+++ packages/shared-ui/src/components/OfferSheet/components/OfferDetailsErrorPage/OfferDetailsErrorPage.spec.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { PlatformAdapterProvider, useMockPlatformAdapter } from 'src/adapters';
 import OfferDetailsErrorPage from '.';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+import { useHydrateAtoms } from 'jotai/utils';
+import { Atom, Provider, WritableAtom } from 'jotai';
+import { offerSheetAtom } from '../../store';
+
+const mockCompanyId = 4242;
 
 jest.mock('next/router', () => ({
   useRouter: () => ({
@@ -12,6 +17,29 @@ jest.mock('next/router', () => ({
     push: jest.fn(),
   }),
 }));
+
+const HydrateAtoms = ({
+  initialValues,
+  children,
+}: {
+  initialValues: any;
+  children: React.ReactNode;
+}) => {
+  useHydrateAtoms(initialValues);
+  return children;
+};
+
+const TestProvider = ({
+  initialValues,
+  children,
+}: {
+  initialValues: any;
+  children: React.ReactNode;
+}) => (
+  <Provider>
+    <HydrateAtoms initialValues={initialValues}>{children}</HydrateAtoms>
+  </Provider>
+);
 
 describe('smoke test', () => {
   it('should render component without error', () => {
@@ -34,5 +62,24 @@ describe('smoke test', () => {
     expect(getByRole('heading', { name: /error loading offer/i })).toBeTruthy();
     expect(getByText(/you can still get to your offer by clicking the button below\./i));
     expect(getByRole('button')).toBeTruthy();
+  });
+
+  it('should navigate to the company page when clicking the company CTA', () => {
+    const mockPlatformAdapter = useMockPlatformAdapter();
+    const { getByRole } = render(
+      <PlatformAdapterProvider adapter={mockPlatformAdapter}>
+        <TestProvider
+          initialValues={[[offerSheetAtom, { offerMeta: { companyId: mockCompanyId } }]]}
+        >
+          <OfferDetailsErrorPage />
+        </TestProvider>
+      </PlatformAdapterProvider>,
+    );
+    fireEvent.click(getByRole('button'));
+
+    expect(mockPlatformAdapter.navigate).toHaveBeenCalledTimes(1);
+    expect(mockPlatformAdapter.navigate).toHaveBeenCalledWith(
+      `/offerdetails.php?cid=${mockCompanyId}`,
+    );
   });
 });

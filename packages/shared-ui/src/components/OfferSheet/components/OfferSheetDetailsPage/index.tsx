97f175e815e29af32b7ccec8d93363e6ef9c882d
cat-blc <160228119+cat-blc@users.noreply.github.com>
2024-06-18 12:24:55 +0100

    chore: adds logs to debug redirect on offersheet (#1613)

--- packages/shared-ui/src/components/OfferSheet/components/OfferSheetDetailsPage/index.tsx
+++ packages/shared-ui/src/components/OfferSheet/components/OfferSheetDetailsPage/index.tsx
@@ -96,6 +96,7 @@ const OfferSheetDetailsPage: FC = () => {
     }
   };
   async function copyCodeAndRedirect(code: string | undefined, url: string | undefined) {
+    console.log('copyCodeAndRedirect values', { code, url });
     if (code) {
       await platformAdapter.writeTextToClipboard(code);
     }
@@ -105,16 +106,21 @@ const OfferSheetDetailsPage: FC = () => {
   }
 
   const handleRedirect = (url: string) => {
+    console.log('handleRedirect url running', url);
     const windowHandle = platformAdapter.navigateExternal(url, { target: 'blank' });
+    console.log('windowHandle.isOpen()', windowHandle.isOpen());
     // This "double" navigation is in place to handle iOS not opening on the first attempt
     if (!windowHandle.isOpen()) {
+      console.log('double navigation running');
       platformAdapter.navigateExternal(url, { target: 'blank' });
     }
 
     // If the window failed to open, navigate in the same tab
     if (!windowHandle.isOpen()) {
+      console.log('second if running');
       platformAdapter.navigateExternal(url, { target: 'blank' });
     } else {
+      console.log('else running to redirect on the same tab');
       // Check if the window was closed by an adblocker and fallback to navigating in the same tab
       setTimeout(() => {
         if (!windowHandle.isOpen()) {

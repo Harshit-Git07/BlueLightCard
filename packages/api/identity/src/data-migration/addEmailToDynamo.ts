92a719f7f4e86516937f86f5e1ede51de6bcd399
salaracu <141646430+salaracu@users.noreply.github.com>
2024-06-25 14:51:37 +0100

    feat: add script to add email and email validated data to dynamodb us… (#1539)
    * feat: add script to add email and email validated data to dynamodb using a csv file
    
    * Update script to handle duplicate and read from env file
    
    * Update rowcount
    
    * Remove comment
    
    * Store successful items ID to file
    
    * Fix function name typo
    
    * consistent file names
    
    * update script to use MySQL call instead of fetching content from CSV
    
    * Enhance the script
    
    * Remove csv parse dependency
    
    * Update script to save total successful items and some further cleanup
    
    * Add confirmed field, track failed items properly

---
+++ packages/api/identity/src/data-migration/addEmailToDynamo.ts
@@ -0,0 +1,182 @@
+
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, UpdateCommand, UpdateCommandInput, QueryCommand } from '@aws-sdk/lib-dynamodb';
+import { promises as fs} from 'fs'
+import mysql from 'mysql2/promise';
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: __dirname+'/.env' });
+
+const region = process.env.REGION ?? 'eu-west-2';
+const tableName = process.env.DYNAMO_TABLE;
+
+const host = process.env.DB_HOST;
+const legacyTable = process.env.LEGACY_TABLE_NAME ?? '';
+const port = parseInt(process.env.DB_PORT ?? '3306');
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DATABASE;
+
+const failedItemsFileName = `itemsFailed-${Date.now().toString()}.txt`;
+const itemsNotFoundFileName = `itemsNotFound-${Date.now().toString()}.txt`;
+const duplicateItemsFileName = `itemsDuplicate-${Date.now().toString()}.txt`;
+const offsetRecordFile = `finalOffset.txt`;
+
+const dynamoclient = new DynamoDBClient({ region: region });
+const dynamodb = DynamoDBDocumentClient.from(dynamoclient); 
+let processedItems = 0;
+let failed = 0;
+
+async function addEmailData() {
+
+  if (host === '' || password === '' || user === '' || tableName === '' || legacyTable === ''){
+    return { status: 'error ', message:  'env variables missing'};
+  }
+
+  const connection = await mysql.createConnection({
+    host: host,
+    port: port,
+    user: user,
+    password: password,
+    database: database
+  });
+
+  let batchNumber = 0;
+  const batchSize: number = parseInt(process.env.BATCH_SIZE ?? '2000') ?? 2000;
+  let offset: number = parseInt(process.env.OFFSET ?? '0') ?? 0;
+
+  while(true){
+    const query = fetchDBRecords(batchSize, offset);
+    const [result] = await connection.query(query);
+    
+    let rows: RowData[] = [];
+    
+    if (Array.isArray(result) && result.length > 0) {
+      rows = result as RowData[];
+    }
+  
+    const promises = [];
+
+    if (rows.length === 0) {
+      break;
+    }
+    for (const row of rows) {
+      const { valid, reason } = validateData(row);
+
+      if (!valid) {
+        await fs.writeFile(failedItemsFileName, `Failed to update item with id: ${row.id} - uuid: ${row.uuid}\n`, { flag: 'a' });
+        failed++;
+        continue;
+      } else {
+        promises.push(await addDataToDynamo(row));
+      }
+
+    }
+    
+
+    // execute Promise.all on promises with a catch on error
+    if (promises.length > 0) {
+      await Promise.all(promises)
+      .then(() => {console.log(`Processed total ${++batchNumber} batches - Processed Items: ${processedItems} - failed Items: ${failed} - current offset: ${offset}`);})
+      .catch((error) => {
+        console.error(`Failed to add item to dynamo ${error as string}`);
+      });
+    }
+
+    offset += batchSize;
+    await fs.writeFile(offsetRecordFile, offset.toString());
+  }
+  await connection.end();
+  return { status: 'success', message: `total processed items ${processedItems}, total failed ${failed}` };
+}
+
+async function addDataToDynamo(item: RowData) {
+
+  if(item.uuid !== null && item.uuid !== undefined && item.uuid !== "" && item.uuid !== "uuid") {
+    const sk = await findSecondaryKey(item.uuid);
+
+    if(sk !== undefined) {
+
+      const updateParams: UpdateCommandInput = {
+        TableName: tableName,
+        Key: {
+          "pk"   : `MEMBER#${item.uuid}`,
+          "sk"  : `${sk}`
+        },
+        UpdateExpression: 'set email = :email, email_validated = :email_validated, confirmed = :confirmed',
+        ExpressionAttributeValues: {':email' : item.email, 
+                                    ':email_validated' : item.validated,
+                                    ':confirmed' : item.confirmed          
+                                    },
+        ReturnValues: "UPDATED_NEW",
+      }
+
+      try {
+        const result = await dynamodb.send(new UpdateCommand(updateParams));
+        processedItems++
+      } catch (err: any) {
+        await fs.writeFile(failedItemsFileName, `Failed to update ${item.uuid}\n`, { flag: 'a' });
+      }
+    }
+  } 
+}
+
+async function findSecondaryKey(uuid: string) {
+  const queryParams = {
+    TableName: tableName,
+    KeyConditionExpression: '#pk= :pk And begins_with(#sk, :sk)',
+    ExpressionAttributeValues: {
+      ':pk': `MEMBER#${uuid}`,
+      ':sk': `PROFILE#`,
+    },
+    ExpressionAttributeNames: {
+      '#pk': 'pk',
+      '#sk': 'sk',
+    },
+  };
+
+  const result = await dynamodb.send(new QueryCommand(queryParams));
+  if(result.Items === null || result.Items?.length === 0) {
+    await fs.writeFile(itemsNotFoundFileName, `${uuid}\n`, { flag: 'a' });
+    failed++;
+    return undefined;
+  } else if(result.Items !== undefined && result.Items?.length > 1) {
+    await fs.writeFile(duplicateItemsFileName, `${uuid}\n`, { flag: 'a' });
+    failed++;
+    return undefined;
+  } else {
+    return (result.Items?.at(0) as Record<string, string>).sk;
+  }
+}
+
+function validateData(rowData: RowData): { valid: any; reason?: any; } {
+  if (rowData.id === null || rowData.id === undefined)
+    return { valid: false, reason: 'legacy user id is null or undefined' };
+  else if (rowData.uuid === null || rowData.uuid === undefined)
+    return { valid: false, reason: 'user uuid is null or undefined' };
+  else if (rowData.email === null || rowData.email === undefined || rowData.email === '')
+    return { valid: false, reason: 'Email is null or undefined' };
+  else if (rowData.validated === null || rowData.validated === undefined)
+    return { valid: false, reason: 'validated is null or undefined' };
+  else if (rowData.confirmed === null || rowData.confirmed === undefined)
+    return { valid: false, reason: 'confirmed is null or undefined' };
+
+  return { valid: true };
+}
+
+function fetchDBRecords(batchSize: number, offset: number) {
+  return(`SELECT u.id, u.uuid, u.email, u.validated, u.confirmed
+  FROM ${legacyTable} u 
+  ORDER BY u.id 
+  LIMIT ${batchSize} OFFSET ${offset}`);
+}
+
+interface RowData {
+  id: number;
+  uuid: string;
+  email: string;
+  validated: number;
+  confirmed: number;
+}
+
+addEmailData();

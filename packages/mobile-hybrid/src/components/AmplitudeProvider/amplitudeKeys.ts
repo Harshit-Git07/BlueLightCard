63ba16b63150aada35e74d7bd30681d8a9338790
Cain Ashmore <cain.ashmore@gmail.com>
2024-06-27 12:42:19 +0100

    fix: Added redemption type feature flag keys to mobile-hybrid

--- packages/mobile-hybrid/src/components/AmplitudeProvider/amplitudeKeys.ts
+++ packages/mobile-hybrid/src/components/AmplitudeProvider/amplitudeKeys.ts
@@ -5,6 +5,10 @@ export enum FeatureFlags {
   SEARCH_START_PAGE_OFFERS_NEAR_YOU_LINK = 'search-start-page-offers-near-you-link',
   SEARCH_RECENT_SEARCHES = 'search-recent-searches',
   V5_API_INTEGRATION = 'v5-api-integration',
+  OFFER_SHEET_VAULT_QR = 'offer-sheet-redeem-qr-app',
+  OFFER_SHEET_GENERIC = 'offer-sheet-redeem-generic-app',
+  OFFER_SHEET_SHOW_CARD = 'offer-sheet-redeem-show-card-app',
+  OFFER_SHEET_PREAPPLIED = 'offer-sheet-redeem-preapplied-app',
 }
 
 export enum Experiments {
@@ -17,7 +21,6 @@ export enum Experiments {
   HOMEPAGE_POSITIONING = 'homepage-positioning',
   BF_FLEXI = 'bf-flexi',
   OFFER_SHEET_VAULT = 'offer-sheet-redeem-vault-app',
-  OFFER_SHEET_GENERIC = 'offer-sheet-redeem-generic-app',
   CATEGORY_LEVEL_THREE_SEARCH = 'category_level_three_search_hybrid',
 }
 

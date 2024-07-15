fb984a1d9de4cd7a3162bc30a7afee1d8893911e
luis-blc <160228623+luis-blc@users.noreply.github.com>
2024-07-15 16:00:43 +0100

    feat: added typography to Badge component (#1762)
    * added typography to Badge component
    
    * fix typo

--- packages/shared-ui/src/components/Badge/index.tsx
+++ packages/shared-ui/src/components/Badge/index.tsx
@@ -14,14 +14,17 @@ const Badge: FC<Props> = ({ label, color, size, platform }) => {
   const dynCss = useCSSConditional({
     'desktop:top-2 desktop:left-2': platform === PlatformVariant.Web,
   });
-  const commonCss = `w-fit rounded-tl-lg rounded-br-lg flex items-center justify-center font-semibold font-museo text-badge-label-colour-light dark:text-badge-label-colour-dark`;
+  const commonCss = `w-fit rounded-tl-lg rounded-br-lg flex items-center justify-center font-badge-large-label-font text-badge-label-colour-light dark:text-badge-label-colour-dark`;
 
   const largeCss = useCSSMerge(
-    'absolute top-0 left-0 mobile:px-3 mobile:py-1 laptop:px-6 laptop:py-2 mobile:text-badge-small-label-font laptop:text-badge-large-label-font',
+    'absolute top-0 left-0 mobile:px-3 mobile:py-1 laptop:px-6 laptop:py-2 mobile:text-badge-small-label-font laptop:text-badge-large-label-font mobile:font-badge-small-label-font-weight mobile:tracking-badge-small-label-font mobile:leading-badge-small-label-font laptop:font-badge-large-label-font-weight laptop:tracking-badge-large-label-font laptop:leading-badge-large-label-font',
     commonCss,
     dynCss,
   );
-  const smallCss = useCSSMerge('px-2 py-0.5 text-badge-small-label-font', commonCss);
+  const smallCss = useCSSMerge(
+    'px-2 py-0.5 text-badge-small-label-font font-badge-small-label-font-weight tracking-badge-small-label-font leading-badge-small-label-font',
+    commonCss,
+  );
 
   return (
     <div

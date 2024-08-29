c732568dd374c2484c9560df4e0512d0a737c550
RuanGomsSG <115535342+RuanGomsSG@users.noreply.github.com>
2024-08-29 14:20:37 +0100

    fix: add backward compatibility (#2126)
    * fix: add backward compatibility
    
    * fix: add backward compatibility for admin apis
    
    * feat: add comment for update blc uk later
    
    * feat: use brand format on blc uk admin api domains; add comment to update blc uk staging domain later

--- packages/api/redemptions/infrastructure/constants/domains.ts
+++ packages/api/redemptions/infrastructure/constants/domains.ts
@@ -1,11 +1,11 @@
 export const productionDomainNames = {
-  BLC_UK: 'redemptions-blc-uk.blcshine.io',
+  BLC_UK: 'redemptions.blcshine.io', // TODO: Update this later to follow below format (backward compatibility required)
   BLC_AU: 'redemptions-blc-au.blcshine.io',
   DDS_UK: 'redemptions-dds-uk.blcshine.io',
 };
 
 export const stagingDomainNames = {
-  BLC_UK: 'staging-redemptions-blc-uk.blcshine.io',
+  BLC_UK: 'staging-redemptions.blcshine.io', // TODO: Update this later to follow below format (backward compatibility required)
   BLC_AU: 'staging-redemptions-blc-au.blcshine.io',
   DDS_UK: 'staging-redemptions-dds-uk.blcshine.io',
 };

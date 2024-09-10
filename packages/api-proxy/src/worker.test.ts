f836eee292dcce20dd7119cc6c11143640c10442
lilyboughton <89781299+lilyboughton@users.noreply.github.com>
2024-09-10 09:10:59 +0100

    feat: [TR-650]  get gatewayUrl using brand headers (#2117)
    * feat: WIP - add functionality to get gatewayUrl using brand headers
    
    * feat: update tests to be more descriptive and more exhaustive
    
    * fix: update tests to meet sonarcloud duplication standards
    
    * fix: adds test for forwarding params to api
    
    * fix: reduce duplicate code
    
    * fix: test tidy up
    
    * fix: add test for brand header case insensitivity
    
    * fix: changes from code review
    
    * fix: refactor based on pr comments
    
    * chore: [TR-650] expand unit test coverage
    
    * chore: resolve merge conflicts
    
    * chore: update based on review feedback
    
    * chore: tidy types and simplify url generation
    
    * chore: remove accidental import
    
    ---------
    
    Co-authored-by: Dominic Lamb <dominiclamb@bluelightcard.co.uk>

--- packages/api-proxy/src/worker.test.ts
+++ packages/api-proxy/src/worker.test.ts
@@ -1,75 +1,271 @@
 import nock from 'nock';
-import { expect, test } from 'vitest';
+import { expect, describe, it } from 'vitest';
 import { Env, handleRequest } from './worker';
 
 const workerDomain = 'https://api.bluelightcard.workers.dev';
 const testEnv: Env = {
-  ENVIRONMENT: 'test',
-  AUTH_API_BLC_UK: 'https://auth.blcshine.io',
-  AUTH_API_BLC_AU: 'https://auth-au.blcshine.io',
-  AUTH_API_DDS_UK: 'https://auth-dds.blcshine.io',
-  IDENTITY_API_BLC_UK: 'https://identity.blcshine.io',
-  IDENTITY_API_BLC_AU: 'https://identity-au.blcshine.io',
-  OFFERS_API_BLC_UK: 'https://offers.blcshine.io',
-  OFFERS_API_BLC_AU: 'https://offers-au.blcshine.io',
-  OFFERS_API_DDS_UK: 'https://offers-dds-uk.blcshine.io',
-  REDEMPTIONS_API_UK: 'https://redemptions.blcshine.io',
-	DISCOVERY_API_UK: 'https://discovery.blcshine.io',
+	ENVIRONMENT: 'test',
+	AUTH_API_BLC_UK: 'https://auth.blc.uk',
+	AUTH_API_BLC_AU: 'https://auth.blc.au',
+	AUTH_API_DDS_UK: 'https://auth.dds.uk',
+	IDENTITY_API_BLC_UK: 'https://identity.blc.uk',
+	IDENTITY_API_BLC_AU: 'https://identity.blc.au',
+	IDENTITY_API_DDS_UK: 'https://identity.dds.uk',
+	OFFERS_API_BLC_UK: 'https://offers.blc.uk',
+	OFFERS_API_BLC_AU: 'https://offers.blc.au',
+	OFFERS_API_DDS_UK: 'https://offers.dds.uk',
+	REDEMPTIONS_API_BLC_UK: 'https://redemptions.blc.uk',
+	REDEMPTIONS_API_BLC_AU: 'https://redemptions.blc.au',
+	REDEMPTIONS_API_DDS_UK: 'https://redemptions.dds.uk',
+	DISCOVERY_API_BLC_UK: 'https://discovery.blc.uk',
+	DISCOVERY_API_BLC_AU: 'https://discovery.blc.au',
+	DISCOVERY_API_DDS_UK: 'https://discovery.dds.uk',
 };
 
-test.each([
-	{ api: testEnv.AUTH_API_BLC_UK, pathPart: '/user', route: '/eu/auth/user' },
-	{ api: testEnv.AUTH_API_BLC_AU, pathPart: '/user', route: '/au/auth/user' },
-	{ api: testEnv.AUTH_API_DDS_UK, pathPart: '/user', route: '/eu/auth/dds/user' },
-	{ api: testEnv.IDENTITY_API_BLC_UK, pathPart: '/member', route: '/eu/identity/member' },
-	{ api: testEnv.IDENTITY_API_BLC_AU, pathPart: '/member', route: '/au/identity/member' },
-	{ api: testEnv.OFFERS_API_BLC_UK, pathPart: '/company', route: '/eu/offers/company' },
-	{ api: testEnv.OFFERS_API_BLC_AU, pathPart: '/company', route: '/au/offers/company' },
-	{ api: testEnv.OFFERS_API_DDS_UK, pathPart: '/company', route: '/eu/offers/dds/company' },
-	{ api: testEnv.REDEMPTIONS_API_UK, pathPart: '/redeem', route: '/eu/redemptions/redeem' }
-])(`(POST) Proxy API: $api, Route:  $route`, (async (testParams): Promise<void> => {
+describe('worker', () => {
+	describe('handleRequest with brand header', () => {
+		it.each([
+			{
+				route: '/auth/auth-endpoint',
+				expectedDomain: 'https://auth.blc.uk',
+				brandHeader: 'BLC_UK',
+				expectedPath: '/auth-endpoint',
+			},
+			{
+				route: '/auth/auth-endpoint',
+				expectedDomain: 'https://auth.blc.au',
+				brandHeader: 'BLC_AU',
+				expectedPath: '/auth-endpoint',
+			},
+			{
+				route: '/auth/auth-endpoint',
+				expectedDomain: 'https://auth.dds.uk',
+				brandHeader: 'DDS_UK',
+				expectedPath: '/auth-endpoint',
+			},
+			{
+				route: '/identity/identity-endpoint',
+				expectedDomain: 'https://identity.blc.uk',
+				brandHeader: 'BLC_UK',
+				expectedPath: '/identity-endpoint',
+			},
+			{
+				route: '/identity/identity-endpoint',
+				expectedDomain: 'https://identity.blc.au',
+				brandHeader: 'BLC_AU',
+				expectedPath: '/identity-endpoint',
+			},
+			{
+				route: '/identity/identity-endpoint',
+				expectedDomain: 'https://identity.dds.uk',
+				brandHeader: 'DDS_UK',
+				expectedPath: '/identity-endpoint',
+			},
+			{
+				route: '/offers/offers-endpoint',
+				expectedDomain: 'https://offers.blc.uk',
+				brandHeader: 'BLC_UK',
+				expectedPath: '/offers-endpoint',
+			},
+			{
+				route: '/offers/offers-endpoint',
+				expectedDomain: 'https://offers.blc.au',
+				brandHeader: 'BLC_AU',
+				expectedPath: '/offers-endpoint',
+			},
+			{
+				route: '/offers/offers-endpoint',
+				expectedDomain: 'https://offers.dds.uk',
+				brandHeader: 'DDS_UK',
+				expectedPath: '/offers-endpoint',
+			},
+			{
+				route: '/redemptions/redeem',
+				expectedDomain: 'https://redemptions.blc.uk',
+				brandHeader: 'BLC_UK',
+				expectedPath: '/redeem',
+			},
+			{
+				route: '/redemptions/redeem',
+				expectedDomain: 'https://redemptions.blc.au',
+				brandHeader: 'BLC_AU',
+				expectedPath: '/redeem',
+			},
+			{
+				route: '/redemptions/redeem',
+				expectedDomain: 'https://redemptions.dds.uk',
+				brandHeader: 'DDS_UK',
+				expectedPath: '/redeem',
+			},
+			{
+				route: '/discovery/discovery-endpoint',
+				expectedDomain: 'https://discovery.blc.uk',
+				brandHeader: 'BLC_UK',
+				expectedPath: '/discovery-endpoint',
+			},
+			{
+				route: '/discovery/discovery-endpoint',
+				expectedDomain: 'https://discovery.blc.au',
+				brandHeader: 'BLC_AU',
+				expectedPath: '/discovery-endpoint',
+			},
+			{
+				route: '/discovery/discovery-endpoint',
+				expectedDomain: 'https://discovery.dds.uk',
+				brandHeader: 'DDS_UK',
+				expectedPath: '/discovery-endpoint',
+			},
+		])('maps request to brand environment: $brandHeader for route: $route', async (testParams) => {
+			nock(testParams.expectedDomain).post(testParams.expectedPath).reply(200);
+			const response = await handleRequest(
+				new Request(`${workerDomain}${testParams.route}`, {
+					method: 'POST',
+					headers: {
+						'x-brand': testParams.brandHeader,
+					},
+				}),
+				testEnv
+			);
 
-	//API Gateway URL, the pathPart that is forwarded to the API Gateway
-	nock(testParams.api).post(testParams.pathPart).reply(200);
+			expect(response.status).toBe(200);
+		});
 
-	//'route' is the path part that is mapped to the API Gateway and removed
-	const response = await handleRequest(new Request(`${workerDomain}${testParams.route}`, { method: 'POST' }), testEnv);
+		it('forwards parameters through to the api', async () => {
+			nock('https://redemptions.blc.uk').post('/redeem').query({ myParam: 'something' }).reply(200);
+			const response = await handleRequest(
+				new Request(`${workerDomain}/redemptions/redeem?myParam=something`, {
+					method: 'POST',
+					headers: {
+						'x-brand': 'BLC_UK',
+					},
+				}),
+				testEnv
+			);
 
-	//if we get a 200, then the mocked URL has been requested therefore the redirect has been successful
-	expect(response.status).toBe(200);
-}));
+			expect(response.status).toBe(200);
+		});
 
-test.each([
-	{
-		api: testEnv.REDEMPTIONS_API_UK,
-		pathPart: '/redemptionDetails',
-		route: '/eu/redemptions/redemptionDetails',
-		queryString: '?offerId=123456'
-	}
-])(`(GET) Proxy API: $api, Route:  $route`, (async (testParams): Promise<void> => {
+		it('processes brand header case insensitively', async () => {
+			nock('https://redemptions.blc.uk').post('/redeem').reply(200);
+			const response = await handleRequest(
+				new Request(`${workerDomain}/redemptions/redeem`, {
+					method: 'POST',
+					headers: {
+						'X-bRaNd': 'BLC_UK',
+					},
+				}),
+				testEnv
+			);
 
-	//API Gateway URL, the pathPart that is forwarded to the API Gateway
-	nock(testParams.api).get(`${testParams.pathPart}${testParams.queryString}`).reply(200);
+			expect(response.status).toBe(200);
+		});
 
-	//'route' is the path part that is mapped to the API Gateway and removed
-	const response = await handleRequest(
-		new Request(`${workerDomain}${testParams.route}${testParams.queryString}`,
-			{ method: 'GET' }),
-		testEnv
-	);
+		it('throws an error when an invalid brand is provided in the headers', async () => {
+			const response = await handleRequest(
+				new Request(`${workerDomain}/redemptions/redeem`, {
+					method: 'POST',
+					headers: {
+						'x-brand': 'INVALID_BRAND',
+					},
+				}),
+				testEnv
+			);
 
-	//if we get a 200, then the mocked URL has been requested therefore the redirect has been successful
-	expect(response.status).toBe(200);
-}));
+			expect(response.status).toBe(404);
+		});
 
-test('404 Proxy Response', async (): Promise<void> => {
-  /*
-    This domain and path are not mapped.
-  */
-  const response = await handleRequest(new Request(`https://example.com/helloworld`, { method: 'POST' }), testEnv);
+		it('falls back to prefix resolution when no brand header router match can be found', async () => {
+			nock('https://redemptions.blc.uk').post('/redeem').reply(200);
+			const response = await handleRequest(
+				new Request(`${workerDomain}/eu/redemptions/redeem`, {
+					method: 'POST',
+					headers: {
+						'x-brand': 'BLC_UK',
+					},
+				}),
+				testEnv
+			);
 
-  /*
-    If no mapping exists we expect to get a 404.
-  */
-  expect(response.status).toBe(404);
+			expect(response.status).toBe(200);
+		});
+
+		it('throws an error when the route cannot be found', async () => {
+			const response = await handleRequest(
+				new Request(`https://example.com/helloworld`, {
+					method: 'POST',
+					headers: {
+						'x-brand': 'BLC_UK',
+					},
+				}),
+				testEnv
+			);
+
+			expect(response.status).toBe(404);
+		});
+	});
+
+	describe('handleRequest with no brand header', () => {
+		it.each([
+			{ api: testEnv.AUTH_API_BLC_UK, pathPart: '/user', route: '/eu/auth/user' },
+			{ api: testEnv.AUTH_API_BLC_AU, pathPart: '/user', route: '/au/auth/user' },
+			{ api: testEnv.AUTH_API_DDS_UK, pathPart: '/user', route: '/eu/auth/dds/user' },
+			{ api: testEnv.IDENTITY_API_BLC_UK, pathPart: '/member', route: '/eu/identity/member' },
+			{ api: testEnv.IDENTITY_API_BLC_AU, pathPart: '/member', route: '/au/identity/member' },
+			{ api: testEnv.OFFERS_API_BLC_UK, pathPart: '/company', route: '/eu/offers/company' },
+			{ api: testEnv.OFFERS_API_BLC_AU, pathPart: '/company', route: '/au/offers/company' },
+			{ api: testEnv.OFFERS_API_DDS_UK, pathPart: '/company', route: '/eu/offers/dds/company' },
+			{ api: testEnv.REDEMPTIONS_API_BLC_UK, pathPart: '/redeem', route: '/eu/redemptions/redeem' },
+		])(`(POST) maps route: $route to api request: $api`, async (testParams) => {
+			nock(testParams.api).post(testParams.pathPart).reply(200);
+
+			const response = await handleRequest(new Request(`${workerDomain}${testParams.route}`, { method: 'POST' }), testEnv);
+
+			expect(response.status).toBe(200);
+		});
+
+		it.each([
+			{
+				api: testEnv.REDEMPTIONS_API_BLC_UK,
+				pathPart: '/redemptionDetails',
+				route: '/eu/redemptions/redemptionDetails',
+			},
+		])(`(GET) maps route: $route to api request: $api`, async (testParams) => {
+			nock(testParams.api).get(testParams.pathPart).reply(200);
+
+			const response = await handleRequest(new Request(`${workerDomain}${testParams.route}`, { method: 'GET' }), testEnv);
+
+			expect(response.status).toBe(200);
+		});
+
+		it('forwards parameters through to the api', async () => {
+			nock('https://redemptions.blc.uk').post('/redeem').query({ myParam: 'something' }).reply(200);
+			const response = await handleRequest(
+				new Request(`${workerDomain}/eu/redemptions/redeem?myParam=something`, {
+					method: 'POST',
+				}),
+				testEnv
+			);
+
+			expect(response.status).toBe(200);
+		});
+
+		it('throws an error when the route cannot be found', async () => {
+			const response = await handleRequest(new Request(`https://example.com/helloworld`, { method: 'POST' }), testEnv);
+
+			expect(response.status).toBe(404);
+		});
+	});
+
+	describe('error handling', () => {
+		it('throws a 500 error when the fetch request fails', async () => {
+			const response = await handleRequest(
+				new Request(`${workerDomain}/eu/redemptions/redeem`, {
+					method: 'POST',
+				}),
+				{ ...testEnv, REDEMPTIONS_API_BLC_UK: 'this-is-not-the-api-you-are-looking-for' }
+			);
+
+			expect(response.status).toBe(500);
+		});
+	});
 });
--- packages/api-proxy/src/worker.ts
+++ packages/api-proxy/src/worker.ts
@@ -1,76 +1,123 @@
 export interface Routes {
-  [key: string]: string;
+	[key: string]: string;
 }
 
+export interface RoutesWithBrand {
+	[key: string]: { BLC_UK: string; BLC_AU: string; DDS_UK: string };
+}
+
+class NotFoundError extends Error {
+	public readonly name = 'NotFoundError';
+
+	constructor(public readonly message: string = '') {
+		super(message);
+	}
+}
 export interface Env {
-  ENVIRONMENT: string;
-  AUTH_API_BLC_UK: string;
-  AUTH_API_BLC_AU: string;
-  AUTH_API_DDS_UK: string;
-  IDENTITY_API_BLC_UK: string;
-  IDENTITY_API_BLC_AU: string;
-  OFFERS_API_BLC_UK: string;
-  OFFERS_API_BLC_AU: string;
-  OFFERS_API_DDS_UK: string;
-  REDEMPTIONS_API_UK: string;
-  DISCOVERY_API_UK: string;
+	ENVIRONMENT: string;
+	AUTH_API_BLC_UK: string;
+	AUTH_API_BLC_AU: string;
+	AUTH_API_DDS_UK: string;
+	IDENTITY_API_BLC_UK: string;
+	IDENTITY_API_BLC_AU: string;
+	IDENTITY_API_DDS_UK: string;
+	OFFERS_API_BLC_UK: string;
+	OFFERS_API_BLC_AU: string;
+	OFFERS_API_DDS_UK: string;
+	REDEMPTIONS_API_BLC_UK: string;
+	REDEMPTIONS_API_BLC_AU: string;
+	REDEMPTIONS_API_DDS_UK: string;
+	DISCOVERY_API_BLC_UK: string;
+	DISCOVERY_API_BLC_AU: string;
+	DISCOVERY_API_DDS_UK: string;
 }
 
-export function removeRegionAndDomainFromPath(path: string, routes: Routes): string {
-  const stringsToRemove = Object.keys(routes);
+type brand = (typeof brands)[number];
+const brands = ['BLC_UK', 'DDS_UK', 'BLC_AU'] as const;
 
-  // Iterate over each string to remove
-  for (const str of stringsToRemove) {
-    // Replace the string with an empty string
-    path = path.replace(str, '');
-  }
+const isValidBrand = (brandHeader: string): brandHeader is brand => {
+	return brands.includes(brandHeader as brand);
+};
 
-  // Remove any trailing slashes resulting from replacements
-  path = path.replace(/\/+$/, '');
+const getUrlByBrand = (url: URL, env: Env, brand: string): URL => {
+	const { pathname, searchParams } = url;
 
-  return path;
-}
+	if (!isValidBrand(brand)) {
+		throw new NotFoundError('Invalid brand');
+	}
+
+	const routes: RoutesWithBrand = {
+		'/auth': { BLC_UK: env.AUTH_API_BLC_UK, BLC_AU: env.AUTH_API_BLC_AU, DDS_UK: env.AUTH_API_DDS_UK },
+		'/identity': { BLC_UK: env.IDENTITY_API_BLC_UK, BLC_AU: env.IDENTITY_API_BLC_AU, DDS_UK: env.IDENTITY_API_DDS_UK },
+		'/offers': { BLC_UK: env.OFFERS_API_BLC_UK, BLC_AU: env.OFFERS_API_BLC_AU, DDS_UK: env.OFFERS_API_DDS_UK },
+		'/redemptions': { BLC_UK: env.REDEMPTIONS_API_BLC_UK, BLC_AU: env.REDEMPTIONS_API_BLC_AU, DDS_UK: env.REDEMPTIONS_API_DDS_UK },
+		'/discovery': { BLC_UK: env.DISCOVERY_API_BLC_UK, BLC_AU: env.DISCOVERY_API_BLC_AU, DDS_UK: env.DISCOVERY_API_DDS_UK },
+	};
+
+	const params = searchParams.size > 0 ? `?${searchParams}` : '';
+	const matchedRoute = Object.keys(routes).find((route) => pathname.startsWith(route));
+
+	if (!matchedRoute) {
+		return getUrlByPrefix(url, env);
+	}
+
+	const baseUrlWithBrand = routes[matchedRoute][brand];
+	const path = pathname.replace(new RegExp(`^${matchedRoute}`), '');
+
+	return new URL(`${baseUrlWithBrand}${path}${params}`);
+};
+
+const getUrlByPrefix = (url: URL, env: Env): URL => {
+	const { pathname, searchParams } = url;
+
+	const routes: Routes = {
+		'/eu/auth/dds': env.AUTH_API_DDS_UK,
+		'/eu/auth': env.AUTH_API_BLC_UK,
+		'/au/auth': env.AUTH_API_BLC_AU,
+		'/eu/identity': env.IDENTITY_API_BLC_UK,
+		'/au/identity': env.IDENTITY_API_BLC_AU,
+		'/eu/offers/dds': env.OFFERS_API_DDS_UK,
+		'/eu/offers': env.OFFERS_API_BLC_UK,
+		'/au/offers': env.OFFERS_API_BLC_AU,
+		'/eu/redemptions': env.REDEMPTIONS_API_BLC_UK,
+		'/eu/discovery': env.DISCOVERY_API_BLC_UK,
+	};
+
+	const params = searchParams.size > 0 ? `?${searchParams}` : '';
+
+	const matchedRoute = Object.keys(routes).find((route) => {
+		return pathname.startsWith(route);
+	});
+
+	if (!matchedRoute) {
+		throw new NotFoundError('Invalid URL');
+	}
+
+	const baseUrl = routes[matchedRoute];
+	const path = pathname.replace(new RegExp(`^${matchedRoute}`), '');
+
+	return new URL(`${baseUrl}${path}${params}`);
+};
+
+const getGatewayUrl = (url: URL, env: Env, brand: string | null) => {
+	return brand ? getUrlByBrand(url, env, brand) : getUrlByPrefix(url, env);
+};
 
 export async function handleRequest(request: Request, env: Env): Promise<Response> {
-  const url = new URL(request.url);
-  const { pathname, searchParams } = url;
+	const url = new URL(request.url);
+	const brand = request.headers.get('x-brand');
 
-  // Define mappings between paths and API Gateway URLs
-  const routes: Routes = {
-    '/eu/auth/dds': env.AUTH_API_DDS_UK,
-    '/eu/auth': env.AUTH_API_BLC_UK,
-    '/au/auth': env.AUTH_API_BLC_AU,
-    '/eu/identity': env.IDENTITY_API_BLC_UK,
-    '/au/identity': env.IDENTITY_API_BLC_AU,
-    '/eu/offers/dds': env.OFFERS_API_DDS_UK,
-    '/eu/offers': env.OFFERS_API_BLC_UK,
-    '/au/offers': env.OFFERS_API_BLC_AU,
-    '/eu/redemptions': env.REDEMPTIONS_API_UK,
-    '/eu/discovery': env.DISCOVERY_API_UK,
-  };
-
-  // Determine which API Gateway to forward the request to
-  let gatewayUrl: null | string = null;
-  let params = searchParams.size > 0 ? `?${searchParams}` : '';
-
-  for (const route in routes) {
-    if (pathname.startsWith(route)) {
-      gatewayUrl = routes[route] + removeRegionAndDomainFromPath(pathname, routes) + params;
-      break;
-    }
-  }
-
-  // If no matching route found, return a 404 response
-  if (!gatewayUrl) {
-    return new Response('Not Found', { status: 404 });
-  }
-
-  // Forward the request to the selected API Gateway
-  return fetch(gatewayUrl, request);
+	try {
+		const gatewayUrl = getGatewayUrl(url, env, brand);
+		return await fetch(gatewayUrl, request);
+	} catch (e) {
+		if (e instanceof NotFoundError) return new Response('Not Found', { status: 404 });
+		return new Response('Internal Server Error', { status: 500 });
+	}
 }
 
 export default {
-  async fetch(request: Request, env: Env): Promise<Response> {
-    return handleRequest(request, env);
-  },
+	async fetch(request: Request, env: Env): Promise<Response> {
+		return handleRequest(request, env);
+	},
 };

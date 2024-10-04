f0981a5b9709a1fd42d06f7f6c338355f1a604f9
ivoblc <163521007+ivoblc@users.noreply.github.com>
2024-10-04 10:27:50 +0200

    fix: TO-1079 Offer sheet: Loader is not seen in dark mode (#2394)
    * fix/TO-1079 Offer sheet: Loader is not seen in dark mode
    
    * fix/TO-1079 Offer sheet: Loader dark mode issue - Unit test snap updated

--- packages/shared-ui/src/components/LoadingSpinner/index.tsx
+++ packages/shared-ui/src/components/LoadingSpinner/index.tsx
@@ -8,7 +8,10 @@ export interface LoadingPlaceholderProps {
 const LoadingSpinner = ({ containerClassName, spinnerClassName }: LoadingPlaceholderProps) => {
   return (
     <div className={`${containerClassName} flex justify-center`}>
-      <FontAwesomeIcon icon={faSpinner} className={`animate-spin m-auto ${spinnerClassName}`} />
+      <FontAwesomeIcon
+        icon={faSpinner}
+        className={`animate-spin m-auto text-colour-onSurface dark:text-colour-onSurface-dark ${spinnerClassName}`}
+      />
     </div>
   );
 };

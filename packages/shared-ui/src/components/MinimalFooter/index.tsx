dc380293992b68a23562352f171c29d682f71537
Marc McGarry <MarcMcGarry@bluelightcard.co.uk>
2024-10-22 17:29:53 +0100

    feat: [MER-149] Log

--- packages/shared-ui/src/components/MinimalFooter/index.tsx
+++ packages/shared-ui/src/components/MinimalFooter/index.tsx
@@ -8,7 +8,7 @@ const MinimalFooter: FC<MinimalFooterProps> = ({ navItems }) => {
     env.APP_BRAND === 'dds-uk'
       ? `© Defence Discount Service 2012 - ${new Date().getFullYear()} Operated by Blue Light Card Ltd`
       : `©Blue Light Card 2008 - ${new Date().getFullYear()}`;
-
+  console.log('env.APP_BRAND', env.APP_BRAND);
   return (
     <div className="w-[1728px] self-stretch justify-between items-center gap-[16px] px-[160px] pb-[24px] inline-flex bg-colour-surface dark:bg-colour-surface-dark">
       <p className="text-center text-slate-600 text-lg font-typography-body-light font-typography-body-light-weight text-typography-body-light tracking-typography-body-light leading-typography-body-light leading-7 tracking-tight">

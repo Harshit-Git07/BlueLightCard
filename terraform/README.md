c3b3e3fcc2e3755f20c04a36f1a5c5c8e997ce3c
Robertas Reiciunas <accesspc@gmail.com>
2024-10-25 11:02:19 +0100

    feat: PE-243 Initial draft of terraform shared code (#2608)
    * feat: Initial draft of terraform shared code for VPC

---
+++ terraform/README.md
@@ -0,0 +1,5 @@
+# Shared infrastructure
+
+WIP - this is still a work in progress, in development stage.
+
+This folder contains all the shared IaC code required for the BLC2.0.
---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "TFC_WORKSPACE_NAME" {
+  type        = string
+  description = "An error occurs when you are running TF backend other than Terraform Cloud"
+
+  default = ""
+}
+
+variable "app" {
+  type        = string
+  description = "APP name"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+
+  default = "eu-west-2"
+}
+
+variable "stage" {
+  type        = string
+  description = "Stage name"
+}

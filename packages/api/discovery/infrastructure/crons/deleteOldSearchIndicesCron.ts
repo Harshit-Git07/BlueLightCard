e9627abf8ebeea2394e0d7767a5b2917ef33688e
Tom <98548671+tombailey14@users.noreply.github.com>
2024-11-01 10:54:49 +0000

    fix: [NO-TICKET] Update cron schedule in staging (#2719)
    * fix: [NO-TICKET] Update cron schedule in staging
    
    * fix: [NO-TICKET] Update formatting
    
    * fix: [NO-TICKET] Ephemeral check
    
    * fix: [NO-TICKET] Remove ephemeral, update deletion to be every hour in staging

--- packages/api/discovery/infrastructure/crons/deleteOldSearchIndicesCron.ts
+++ packages/api/discovery/infrastructure/crons/deleteOldSearchIndicesCron.ts
@@ -5,13 +5,14 @@ import { isProduction, isStaging } from '@blc-mono/core/utils/checkEnvironment';
 import { CronJobIDs } from '../constants/cronJobIDs';
 
 const EVERY_12_HOURS = 'cron(0 0/12 * * ? *)';
+const EVERY_HOUR = 'rate(1 hour)';
 
 export function deleteOldSearchIndicesCron(stack: Stack, functionDefinition: FunctionDefinition): Cron {
   return new Cron(stack, CronJobIDs.DELETE_OLD_SEARCH_INDICES, {
     job: {
       function: functionDefinition,
     },
-    schedule: EVERY_12_HOURS,
+    schedule: isStaging(stack.stage) ? EVERY_HOUR : EVERY_12_HOURS,
     enabled: isProduction(stack.stage) || isStaging(stack.stage),
   });
 }
--- packages/api/discovery/infrastructure/crons/populateSearchIndexCron.ts
+++ packages/api/discovery/infrastructure/crons/populateSearchIndexCron.ts
@@ -1,15 +1,18 @@
 import { Cron, FunctionDefinition, Stack } from 'sst/constructs';
 
+import { isStaging } from '@blc-mono/core/utils/checkEnvironment';
+
 import { CronJobIDs } from '../constants/cronJobIDs';
 
 const EVERY_HOUR_AT_15_MINUTES_PAST = 'cron(15 * * * ? *)';
+const EVERY_2_MINUTES = 'rate(2 minutes)';
 
 export function populateSearchIndexCron(stack: Stack, functionDefinition: FunctionDefinition): Cron {
   return new Cron(stack, CronJobIDs.POPULATE_SEARCH_INDEX, {
     job: {
       function: functionDefinition,
     },
-    schedule: EVERY_HOUR_AT_15_MINUTES_PAST,
+    schedule: isStaging(stack.stage) ? EVERY_2_MINUTES : EVERY_HOUR_AT_15_MINUTES_PAST,
     enabled: true,
   });
 }

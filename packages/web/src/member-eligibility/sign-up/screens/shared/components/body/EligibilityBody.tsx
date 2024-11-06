e93f3d10a8eab80c40bc9e4491d2ea7c4f088ee5
aidan-instil <aidan.campbell@instil.co>
2024-11-06 11:47:51 +0000

    style: adjust content padding between the header and content (#2773)

--- packages/web/src/member-eligibility/sign-up/screens/shared/components/body/EligibilityBody.tsx
+++ packages/web/src/member-eligibility/sign-up/screens/shared/components/body/EligibilityBody.tsx
@@ -8,7 +8,7 @@ interface MainContainerProps {
 export const EligibilityBody: FC<MainContainerProps> = ({ children, className = '' }) => {
   return (
     <main
-      className={`flex-grow flex flex-col justify-start items-center md:w-[500px] sm:w-full mx-auto ${className}`}
+      className={`flex-grow flex flex-col justify-start items-center md:w-[500px] sm:w-full mx-auto pt-[32px] md:pt-12 ${className}`}
     >
       {children}
     </main>

df1a81a0226183558086b92e89276fe27443f0d8
Neil Armstrong <NeilArmstrong@bluelightcard.co.uk>
2024-11-11 17:42:25 +0000

    feat: Prevent dark mode on eligibility flow (#2855)
    * feat: Prevent darkmode on eligibility flow
    
    * feat: Add comment explaining why dark mode is disabled
    
    * feat: Remove unused imports

--- packages/web/src/pages/eligibility.tsx
+++ packages/web/src/pages/eligibility.tsx
@@ -1,8 +1,12 @@
-import { NextPage } from 'next';
+import { NextPage, Viewport } from 'next';
 import { SignupEligibilityFlow } from '@/root/src/member-eligibility/sign-up/SignupEligibilityFlow';
 
 const Eligibility: NextPage = () => {
   return <SignupEligibilityFlow />;
+};
+
+export const viewport: Viewport = {
+  colorScheme: 'only light', // We are disabling dark mode here as these "responsive" designs (unlike the "web / hybrid" designs) do not support dark mode yet
 };
 
 export default Eligibility;

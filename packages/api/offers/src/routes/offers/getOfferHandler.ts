2da176be4016869cd8ceb18dd8ea20ec970c815e
Cain Ashmore <cain.ashmore@gmail.com>
2024-11-14 10:15:35 +0000

    fix: tech debt - code duplication reduction (#2903)
    * Updated the way the users service is retrieved to use a common function and reduce code duplication

--- packages/api/offers/src/routes/offers/getOfferHandler.ts
+++ packages/api/offers/src/routes/offers/getOfferHandler.ts
@@ -11,7 +11,7 @@ import { checkIfEnvironmentVariablesExist } from '../../utils/validation';
 import { Logger } from '../../../../core/src/utils/logger/logger';
 import { DI_KEYS } from '../../utils/diTokens';
 import { decodeJWT } from '../../utils/decodeJWT';
-import { UserProfile } from '../../services/UserProfile';
+import { getServiceParams } from '../../utils/serviceParams';
 
 let isEnvironmentVariableExist = false;
 const service = getEnvRaw('SERVICE');
@@ -63,16 +63,9 @@ export const handler = async (event: APIGatewayEvent) => {
   let serviceParams = '';
 
   try {
-    const userProfile = new UserProfile(authToken);
-    const userProfileData = await userProfile.getUserProfileRequest();
-    if(userProfileData?.data?.data?.profile?.organisation) {
-      serviceParams = `&service=${userProfileData.data.data.profile.organisation}`; 
-    } else {
-      serviceParams = `&service=`;
-    }
+    serviceParams = await getServiceParams(authToken);
   } catch (error: any) {
     logger.error({ message: 'Error fetching user profile', body: error });
-    logger.error({ message: 'Error fetching company details', body: error });
     return Response.Error(error as Error, HttpStatusCode.INTERNAL_SERVER_ERROR);
   }
 

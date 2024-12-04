4e92c02070371f22df73f1d09eabb03227808668
Tom <98548671+tombailey14@users.noreply.github.com>
2024-12-04 11:23:53 +0000

    feat: [DISCO-1258] Update trust inclusion logic to subtract excluded trusts from included trusts (#3085)

--- packages/api/discovery/application/utils/trustRules.test.ts
+++ packages/api/discovery/application/utils/trustRules.test.ts
@@ -6,8 +6,13 @@ describe('trustRules', () => {
 
     expect(result).toBe(true);
   });
-  it('should return false if the users service is in the excluded trusts', () => {
+  it('should return false if the users service is in the excluded trusts and included trusts is empty', () => {
     const result = isValidTrust('NHS', [], ['NHS']);
+
+    expect(result).toBe(false);
+  });
+  it('should return false when users service is in the included trusts & excluded trusts', () => {
+    const result = isValidTrust('NHS', ['NHS'], ['NHS']);
 
     expect(result).toBe(false);
   });
@@ -16,7 +21,7 @@ describe('trustRules', () => {
 
     expect(result).toBe(true);
   });
-  it('should return true when users service is not in the excluded trusts', () => {
+  it('should return true when users service is not in the excluded trusts and included trusts is empty', () => {
     const result = isValidTrust('NHS', [], ['POLICE']);
 
     expect(result).toBe(true);
--- packages/api/discovery/application/utils/trustRules.ts
+++ packages/api/discovery/application/utils/trustRules.ts
@@ -1,11 +1,8 @@
 export const isValidTrust = (trust: string, includedTrusts: string[], excludedTrusts: string[]): boolean => {
-  if (includedTrusts.includes(trust)) {
-    return true;
-  }
-  if (includedTrusts.length > 0 && !includedTrusts.includes(trust)) {
+  if (excludedTrusts.includes(trust)) {
     return false;
   }
-  if (!excludedTrusts.includes(trust)) {
+  if (includedTrusts.length === 0 || includedTrusts.includes(trust)) {
     return true;
   }
   return false;
